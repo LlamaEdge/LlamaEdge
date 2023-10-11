f1d1fa5053c87e5d69626454effc086b9ac0588b
Xin Liu <sam@secondstate.io>
2023-10-11 11:55:50 +0000

    feat(llm-api-server): implement `llama_models_handler`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/llama-api-server/src/backend/ggml/llama.rs
+++ api-server/llama-api-server/src/backend/ggml/llama.rs
@@ -7,11 +7,61 @@ use xin::{
         ChatCompletionRole, FinishReason,
     },
     common::Usage,
+    models::{ListModelsResponse, Model},
 };
 
-pub(crate) async fn _llama_models_handler() -> Result<Response<Body>, hyper::Error> {
-    println!("llama_models_handler not implemented");
-    error::not_implemented()
+pub(crate) async fn llama_models_handler() -> Result<Response<Body>, hyper::Error> {
+    let llama_2_7b_chat_q5_k_m = Model {
+        id: String::from("llama-2-7b-chat.Q5_K_M.gguf"),
+        created: std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_secs(),
+        object: String::from("model"),
+        owned_by: String::from("https://huggingface.co/TheBloke"),
+    };
+
+    let codellama_13b_instruct_q4_0 = Model {
+        id: String::from("codellama-13b-instruct.Q4_0.gguf"),
+        created: std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_secs(),
+        object: String::from("model"),
+        owned_by: String::from("https://huggingface.co/TheBloke"),
+    };
+
+    let mistral_7b_instruct_v0_1 = Model {
+        id: String::from("Mistral-7B-Instruct-v0.1.gguf"),
+        created: std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_secs(),
+        object: String::from("model"),
+        owned_by: String::from("https://huggingface.co/TheBloke"),
+    };
+
+    let list_models_response = ListModelsResponse {
+        object: String::from("list"),
+        data: vec![
+            llama_2_7b_chat_q5_k_m,
+            codellama_13b_instruct_q4_0,
+            mistral_7b_instruct_v0_1,
+        ],
+    };
+
+    // return response
+    let result = Response::builder()
+        .header("Access-Control-Allow-Origin", "*")
+        .header("Access-Control-Allow-Methods", "*")
+        .header("Access-Control-Allow-Headers", "*")
+        .body(Body::from(
+            serde_json::to_string(&list_models_response).unwrap(),
+        ));
+    match result {
+        Ok(response) => Ok(response),
+        Err(e) => error::internal_server_error(e.to_string()),
+    }
 }
 
 pub(crate) async fn _llama_embeddings_handler() -> Result<Response<Body>, hyper::Error> {
--- api-server/llama-api-server/src/backend/ggml/mod.rs
+++ api-server/llama-api-server/src/backend/ggml/mod.rs
@@ -14,7 +14,7 @@ pub(crate) async fn handle_llama_request(
         }
         // "/v1/completions" => llama::llama_completions_handler().await,
         // "/v1/embeddings" => llama::llama_embeddings_handler().await,
-        // "/v1/models" => llama::llama_models_handler().await,
+        "/v1/models" => llama::llama_models_handler().await,
         _ => error::not_implemented(),
     }
 }
