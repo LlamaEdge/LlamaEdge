0f5993bc67e0ea80dd22c5ce168598a7e92d1b98
Xin Liu <sam@secondstate.io>
2023-10-30 13:41:26 +0800

    feat(simple): add cli options
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- simple/src/main.rs
+++ simple/src/main.rs
@@ -1,33 +1,83 @@
-use std::env;
-use wasi_nn;
+use clap::{Arg, Command};
+use once_cell::sync::OnceCell;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let model_name: &str = &args[1];
-    let prompt: &str = &args[2];
+const DEFAULT_CTX_SIZE: &str = "2048";
+static CTX_SIZE: OnceCell<usize> = OnceCell::new();
 
+fn main() -> Result<(), String> {
+    let matches = Command::new("Llama API Server")
+        .arg(
+            Arg::new("prompt")
+                .short('p')
+                .long("prompt")
+                .value_name("PROMPT")
+                .help("Sets the prompt.")
+                .required(true),
+        )
+        .arg(
+            Arg::new("model_alias")
+                .short('m')
+                .long("model-alias")
+                .value_name("ALIAS")
+                .help("Sets the model alias")
+                .default_value("default"),
+        )
+        .arg(
+            Arg::new("ctx_size")
+                .short('c')
+                .long("ctx-size")
+                .value_parser(clap::value_parser!(u32))
+                .value_name("CTX_SIZE")
+                .help("Sets the prompt context size")
+                .default_value(DEFAULT_CTX_SIZE),
+        )
+        .get_matches();
+
+    // model alias
+    let model_name = matches
+        .get_one::<String>("model_alias")
+        .unwrap()
+        .to_string();
+
+    // prompt context size
+    let ctx_size = matches.get_one::<u32>("ctx_size").unwrap();
+    CTX_SIZE
+        .set(*ctx_size as usize)
+        .expect("Fail to parse prompt context size");
+
+    // prompt
+    let prompt = matches.get_one::<String>("prompt").unwrap().to_string();
+
+    // load the model into wasi-nn
     let graph =
         wasi_nn::GraphBuilder::new(wasi_nn::GraphEncoding::Ggml, wasi_nn::ExecutionTarget::AUTO)
-            .build_from_cache(model_name)
+            .build_from_cache(&model_name)
             .expect("Failed to load the model");
 
+    // initialize the execution context
     let mut context = graph
         .init_execution_context()
         .expect("Failed to init context");
-    let tensor_data = prompt.as_bytes().to_vec();
+
+    // set input tensor
+    let tensor_data = prompt.as_str().as_bytes().to_vec();
     context
         .set_input(0, wasi_nn::TensorType::U8, &[1], &tensor_data)
         .expect("Failed to set prompt as the input tensor");
+
+    // execute the inference
     context.compute().expect("Failed to complete inference");
 
-    let max_output_size = 4096 * 6;
-    let mut output_buffer = vec![0u8; max_output_size];
+    // retrieve the output
+    let mut output_buffer = vec![0u8; *CTX_SIZE.get().unwrap()];
     let mut output_size = context
         .get_output(0, &mut output_buffer)
-        .expect("Failed to get inference output");
-    output_size = std::cmp::min(max_output_size, output_size);
+        .expect("Failed to get output tensor");
+    output_size = std::cmp::min(*CTX_SIZE.get().unwrap(), output_size);
     let output = String::from_utf8_lossy(&output_buffer[..output_size]).to_string();
 
-    println!("{}", prompt);
-    println!("{}", output);
+    println!("\nprompt: {}", &prompt);
+    println!("\noutput: {}", output);
+
+    Ok(())
 }
