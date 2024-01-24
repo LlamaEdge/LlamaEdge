b43a0451f3bf175629ce138cf96c69132f60e8cd
Xin Liu <sam@secondstate.io>
2024-01-24 10:50:03 +0800

    feat(llama-api-server): show plugin version info
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/llama-api-server/src/main.rs
+++ api-server/llama-api-server/src/main.rs
@@ -312,6 +312,23 @@ async fn main() -> Result<(), ServerError> {
         ));
     }
 
+    {
+        let graph = GRAPH.get().unwrap().lock().unwrap();
+
+        // get version info
+        let max_output_size = *MAX_BUFFER_SIZE.get().unwrap();
+        let mut output_buffer = vec![0u8; max_output_size];
+        let mut output_size = graph.get_output(1, &mut output_buffer).unwrap();
+        output_size = std::cmp::min(max_output_size, output_size);
+        let metadata: serde_json::Value =
+            serde_json::from_slice(&output_buffer[..output_size]).unwrap();
+        println!(
+            "[INFO] Plugin version: b{} (commit {})",
+            metadata["llama_build_number"].as_u64().unwrap(),
+            metadata["llama_commit"].as_str().unwrap(),
+        );
+    }
+
     if log_stat || log_all {
         print_log_end_separator(Some("*"), None);
     }
