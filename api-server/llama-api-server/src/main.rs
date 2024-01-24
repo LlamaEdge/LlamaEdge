81b99244a91fc2f5870480d992b91fe1050e884d
Xin Liu <sam@secondstate.io>
2024-01-24 12:42:54 +0800

    feat(llama-api-server): disable `phi-2-chat` and `phi-2-instruct`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/llama-api-server/src/main.rs
+++ api-server/llama-api-server/src/main.rs
@@ -137,8 +137,6 @@ async fn main() -> Result<(), ServerError> {
                     "deepseek-chat",
                     "deepseek-coder",
                     "solar-instruct",
-                    "phi-2-chat",
-                    "phi-2-instruct",
                 ])
                 .value_name("TEMPLATE")
                 .help("Sets the prompt template.")
