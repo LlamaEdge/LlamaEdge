b765c2b8a3ea1d219aece0c43cfa5be7f568cc8c
Xin Liu <sam@secondstate.io>
2024-02-16 16:07:23 +0800

    feat(api-server): integrate `llama-core`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/llama-api-server/src/backend/mod.rs
+++ api-server/llama-api-server/src/backend/mod.rs
@@ -1,14 +1,12 @@
 pub(crate) mod ggml;
 
-use crate::{error, ModelInfo};
+use crate::error;
 use chat_prompts::PromptTemplateType;
 use hyper::{Body, Request, Response};
 
 pub(crate) async fn handle_llama_request(
     req: Request<Body>,
-    model_info: ModelInfo,
     template_ty: PromptTemplateType,
-    created: u64,
     log_prompts: bool,
 ) -> Result<Response<Body>, hyper::Error> {
     match req.uri().path() {
@@ -17,7 +15,7 @@ pub(crate) async fn handle_llama_request(
         }
         "/v1/completions" => ggml::completions_handler(req).await,
         // "/v1/embeddings" => ggml::_embeddings_handler().await,
-        "/v1/models" => ggml::models_handler(model_info, template_ty, created).await,
+        "/v1/models" => ggml::models_handler().await,
         _ => error::invalid_endpoint(req.uri().path()),
     }
 }
