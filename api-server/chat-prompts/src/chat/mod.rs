4201a7a80f08fef3fa9e79a91648df4489edd129
Xin Liu <sam@secondstate.io>
2024-02-25 21:52:02 +0800

    feat(prompts): add `VicunaLlavaPrompt`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/chat-prompts/src/chat/mod.rs
+++ api-server/chat-prompts/src/chat/mod.rs
@@ -48,6 +48,7 @@ pub enum ChatPrompt {
     VicunaChatPrompt,
     /// Vicuna 1.1
     Vicuna11ChatPrompt,
+    VicunaLlavaPrompt,
     ChatMLPrompt,
     Baichuan2ChatPrompt,
     WizardCoderPrompt,
