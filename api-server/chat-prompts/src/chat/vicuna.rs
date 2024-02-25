cea0d33bb5c62ff9ff88538743d2ba23a8f8d84d
Xin Liu <sam@secondstate.io>
2024-02-25 21:52:02 +0800

    feat(prompts): add `VicunaLlavaPrompt`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/chat-prompts/src/chat/vicuna.rs
+++ api-server/chat-prompts/src/chat/vicuna.rs
@@ -189,3 +189,127 @@ impl BuildChatPrompt for Vicuna11ChatPrompt {
         Ok(prompt)
     }
 }
+
+/// Vicuna-1.0 Prompt Template
+#[derive(Debug, Default, Clone)]
+pub struct VicunaLlavaPrompt;
+impl VicunaLlavaPrompt {
+    /// Create a system prompt from a chat completion request message.
+    fn create_system_prompt(&self, message: &ChatCompletionSystemMessage) -> String {
+        let content = message.content();
+        match content.is_empty() {
+            true => String::from("You are a helpful, respectful and honest assistant. Always answer as short as possible, while being safe."),
+            false => format!("{content}"),
+        }
+    }
+
+    /// Create a user prompt from a chat completion request message.
+    fn append_user_message(
+        &self,
+        chat_history: impl AsRef<str>,
+        system_prompt: impl AsRef<str>,
+        message: &ChatCompletionUserMessage,
+    ) -> String {
+        match message.content() {
+            ChatCompletionUserMessageContent::Text(content) => {
+                match chat_history.as_ref().is_empty() {
+                    true => format!(
+                        "{system_prompt}\nUSER: {user_message}",
+                        system_prompt = system_prompt.as_ref().trim(),
+                        user_message = content.trim(),
+                    ),
+                    false => format!(
+                        "{chat_history}\nUSER: {user_message}",
+                        chat_history = chat_history.as_ref().trim(),
+                        user_message = content.trim(),
+                    ),
+                }
+            }
+            ChatCompletionUserMessageContent::Parts(parts) => {
+                let mut content = String::new();
+                let mut image_content = String::new();
+                for part in parts {
+                    match part {
+                        ContentPart::Text(text_content) => {
+                            content.push_str(text_content.text());
+                            content.push_str("\n");
+                        }
+                        ContentPart::Image(_) => {
+                            image_content = String::from("<image>");
+                        }
+                    }
+                }
+
+                match chat_history.as_ref().is_empty() {
+                    true => format!(
+                        "{system_prompt}\nUSER:{image_embeddings}\n{user_message}",
+                        system_prompt = system_prompt.as_ref().trim(),
+                        image_embeddings = image_content.trim(),
+                        user_message = content.trim(),
+                    ),
+                    false => format!(
+                        "{chat_history}\nUSER:{image_embeddings}\n{user_message}",
+                        chat_history = chat_history.as_ref().trim(),
+                        image_embeddings = image_content.trim(),
+                        user_message = content.trim(),
+                    ),
+                }
+            }
+        }
+    }
+
+    /// create an assistant prompt from a chat completion request message.
+    fn append_assistant_message(
+        &self,
+        chat_history: impl AsRef<str>,
+        message: &ChatCompletionAssistantMessage,
+    ) -> Result<String> {
+        let content = match message.content() {
+            Some(content) => content.to_string(),
+            // Note that the content is optional if `tool_calls` is specified.
+            None => match message.tool_calls().is_some() {
+                true => String::new(),
+                false => return Err(PromptError::NoAssistantMessage),
+            },
+        };
+
+        Ok(format!(
+            "{chat_history}\nASSISTANT: {assistant_message}",
+            chat_history = chat_history.as_ref().trim(),
+            assistant_message = content.trim(),
+        ))
+    }
+}
+impl BuildChatPrompt for VicunaLlavaPrompt {
+    fn build(&self, messages: &mut Vec<ChatCompletionRequestMessage>) -> Result<String> {
+        if messages.is_empty() {
+            return Err(crate::error::PromptError::NoMessages);
+        }
+
+        // system prompt
+        let system_prompt = match messages[0] {
+            ChatCompletionRequestMessage::System(ref message) => {
+                self.create_system_prompt(&message)
+            }
+            _ => String::from("You are a helpful, respectful and honest assistant. Always answer as short as possible, while being safe."),
+        };
+
+        // append user/assistant messages
+        let mut prompt = String::new();
+        for message in messages {
+            match message {
+                ChatCompletionRequestMessage::User(message) => {
+                    prompt = self.append_user_message(&prompt, &system_prompt, message);
+                }
+                ChatCompletionRequestMessage::Assistant(message) => {
+                    prompt = self.append_assistant_message(&prompt, message)?;
+                }
+                ChatCompletionRequestMessage::System(_) => continue,
+            }
+        }
+
+        prompt.push_str("\nASSISTANT:");
+
+        Ok(prompt)
+    }
+}
