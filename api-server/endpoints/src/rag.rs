460c064d9f7cd3101c18f739a268f557b69e62b8
Xin Liu <sam@secondstate.io>
2024-02-28 10:28:36 +0800

    feat(endpoints): add `RagChatCompletionRequestBuilder`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/endpoints/src/rag.rs
+++ api-server/endpoints/src/rag.rs
@@ -1,5 +1,5 @@
 use crate::{
-    chat::{ChatCompletionRequest, ChatCompletionRequestMessage},
+    chat::{ChatCompletionRequest, ChatCompletionRequestMessage, ChatCompletionRequestSampling},
     embeddings::EmbeddingRequest,
 };
 use serde::{Deserialize, Serialize};
@@ -13,6 +13,24 @@ pub struct RagEmbeddingRequest {
     pub qdrant_url: String,
     #[serde(rename = "collection_name")]
     pub qdrant_collection_name: String,
+}
+impl RagEmbeddingRequest {
+    pub fn new(
+        input: &[String],
+        qdrant_url: impl AsRef<str>,
+        qdrant_collection_name: impl AsRef<str>,
+    ) -> Self {
+        RagEmbeddingRequest {
+            embedding_request: EmbeddingRequest {
+                model: "dummy-embedding-model".to_string(),
+                input: input.to_vec(),
+                encoding_format: None,
+                user: None,
+            },
+            qdrant_url: qdrant_url.as_ref().to_string(),
+            qdrant_collection_name: qdrant_collection_name.as_ref().to_string(),
+        }
+    }
 }
 
 #[test]
@@ -70,7 +88,7 @@ pub struct RagChatCompletionsRequest {
     pub qdrant_url: String,
     /// The name of the collection in Qdrant.
     pub qdrant_collection_name: String,
-    /// Max number of retrieved result.
+    /// Max number of retrieved results.
     pub limit: u64,
     /// Adjust the randomness of the generated text. Between 0.0 and 2.0. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.
     ///
@@ -138,5 +156,117 @@ impl RagChatCompletionsRequest {
             functions: None,
             function_call: None,
         }
+    }
+}
+
+/// Request builder for creating a new RAG chat completion request.
+pub struct RagChatCompletionRequestBuilder {
+    req: RagChatCompletionsRequest,
+}
+impl RagChatCompletionRequestBuilder {
+    /// Creates a new builder with the given model.
+    ///
+    /// # Arguments
+    ///
+    /// * `model` - ID of the model to use.
+    ///
+    /// * `messages` - A list of messages comprising the conversation so far.
+    ///
+    /// * `sampling` - The sampling method to use.
+    pub fn new(
+        messages: Vec<ChatCompletionRequestMessage>,
+        qdrant_url: impl Into<String>,
+        qdrant_collection_name: impl Into<String>,
+        limit: u64,
+    ) -> Self {
+        Self {
+            req: RagChatCompletionsRequest {
+                chat_model: Some("dummy-chat-model".to_string()),
+                messages,
+                embedding_model: "dummy-embedding-model".to_string(),
+                encoding_format: None,
+                qdrant_url: qdrant_url.into(),
+                qdrant_collection_name: qdrant_collection_name.into(),
+                limit,
+                temperature: None,
+                top_p: None,
+                n_choice: None,
+                stream: None,
+                stop: None,
+                max_tokens: None,
+                presence_penalty: None,
+                frequency_penalty: None,
+                logit_bias: None,
+                user: None,
+            },
+        }
+    }
+
+    pub fn with_sampling(mut self, sampling: ChatCompletionRequestSampling) -> Self {
+        let (temperature, top_p) = match sampling {
+            ChatCompletionRequestSampling::Temperature(t) => (t, 1.0),
+            ChatCompletionRequestSampling::TopP(p) => (1.0, p),
+        };
+        self.req.temperature = Some(temperature);
+        self.req.top_p = Some(top_p);
+        self
+    }
+
+    /// Sets the number of chat completion choices to generate for each input message.
+    ///
+    /// # Arguments
+    ///
+    /// * `n` - How many chat completion choices to generate for each input message. If `n` is less than 1, then sets to `1`.
+    pub fn with_n_choices(mut self, n: i32) -> Self {
+        let n_choice = if n < 1 { 1 } else { n };
+        self.req.n_choice = Some(n_choice);
+        self
+    }
+
+    pub fn with_stream(mut self, flag: bool) -> Self {
+        self.req.stream = Some(flag);
+        self
+    }
+
+    pub fn with_stop(mut self, stop: Vec<String>) -> Self {
+        self.req.stop = Some(stop);
+        self
+    }
+
+    /// Sets the maximum number of tokens to generate in the chat completion. The total length of input tokens and generated tokens is limited by the model's context length.
+    ///
+    /// # Argument
+    ///
+    /// * `max_tokens` - The maximum number of tokens to generate in the chat completion. If `max_tokens` is less than 1, then sets to `16`.
+    pub fn with_max_tokens(mut self, max_tokens: u64) -> Self {
+        let max_tokens = if max_tokens < 1 { 16 } else { max_tokens };
+        self.req.max_tokens = Some(max_tokens);
+        self
+    }
+
+    /// Sets the presence penalty. Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    pub fn with_presence_penalty(mut self, penalty: f64) -> Self {
+        self.req.presence_penalty = Some(penalty);
+        self
+    }
+
+    /// Sets the frequency penalty. Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+    pub fn with_frequency_penalty(mut self, penalty: f64) -> Self {
+        self.req.frequency_penalty = Some(penalty);
+        self
+    }
+
+    pub fn with_logits_bias(mut self, map: HashMap<String, f64>) -> Self {
+        self.req.logit_bias = Some(map);
+        self
+    }
+
+    pub fn with_user(mut self, user: impl Into<String>) -> Self {
+        self.req.user = Some(user.into());
+        self
+    }
+
+    pub fn build(self) -> RagChatCompletionsRequest {
+        self.req
     }
 }
