b9b390d95c9695bb96b030f176fc54ca18340867
Xin Liu <sam@secondstate.io>
2024-03-12 16:56:22 +0900

    refactor(prompts): update `VicunaLlavaPrompt` for base64 image
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/chat-prompts/src/chat/vicuna.rs
+++ api-server/chat-prompts/src/chat/vicuna.rs
@@ -1,9 +1,12 @@
 use super::BuildChatPrompt;
 use crate::error::{PromptError, Result};
+use base64::{engine::general_purpose, Engine as _};
 use endpoints::chat::{
     ChatCompletionAssistantMessage, ChatCompletionRequestMessage, ChatCompletionSystemMessage,
     ChatCompletionUserMessage, ChatCompletionUserMessageContent, ContentPart,
 };
+use image::io::Reader as ImageReader;
+use std::io::Cursor;
 
 /// Vicuna-1.0 Prompt Template
 #[derive(Debug, Default, Clone)]
@@ -209,8 +212,8 @@ impl VicunaLlavaPrompt {
         chat_history: impl AsRef<str>,
         system_prompt: impl AsRef<str>,
         message: &ChatCompletionUserMessage,
-    ) -> String {
-        match message.content() {
+    ) -> Result<String> {
+        let prompt = match message.content() {
             ChatCompletionUserMessageContent::Text(content) => {
                 match chat_history.as_ref().is_empty() {
                     true => format!(
@@ -239,7 +242,11 @@ impl VicunaLlavaPrompt {
                                 true => String::from("<image>"),
                                 false => {
                                     let base64_str = part.image().url.as_str();
-                                    format!(r#"<img src="data:image/jpeg;base64,{}">"#, base64_str)
+                                    let format = is_image_format(base64_str)?;
+                                    format!(
+                                        r#"<img src="data:image/{};base64,{}">"#,
+                                        format, base64_str
+                                    )
                                 }
                             };
                         }
@@ -261,7 +268,9 @@ impl VicunaLlavaPrompt {
                     ),
                 }
             }
-        }
+        };
+
+        Ok(prompt)
     }
 
     /// create an assistant prompt from a chat completion request message.
@@ -305,7 +314,7 @@ impl BuildChatPrompt for VicunaLlavaPrompt {
         for message in messages {
             match message {
                 ChatCompletionRequestMessage::User(message) => {
-                    prompt = self.append_user_message(&prompt, &system_prompt, message);
+                    prompt = self.append_user_message(&prompt, &system_prompt, message)?;
                 }
                 ChatCompletionRequestMessage::Assistant(message) => {
                     prompt = self.append_assistant_message(&prompt, message)?;
@@ -318,4 +327,37 @@ impl BuildChatPrompt for VicunaLlavaPrompt {
 
         Ok(prompt)
     }
+}
+
+fn is_image_format(base64_str: &str) -> Result<String> {
+    let image_data = match general_purpose::STANDARD.decode(base64_str) {
+        Ok(data) => data,
+        Err(_) => {
+            return Err(PromptError::Operation(
+                "Failed to decode base64 string.".to_string(),
+            ))
+        }
+    };
+
+    let format = ImageReader::new(Cursor::new(image_data))
+        .with_guessed_format()
+        .unwrap()
+        .format();
+
+    let image_format = match format {
+        Some(image::ImageFormat::Png) => "png".to_string(),
+        Some(image::ImageFormat::Jpeg) => "jpeg".to_string(),
+        Some(image::ImageFormat::Tga) => "tga".to_string(),
+        Some(image::ImageFormat::Bmp) => "bmp".to_string(),
+        Some(image::ImageFormat::Gif) => "gif".to_string(),
+        Some(image::ImageFormat::Hdr) => "hdr".to_string(),
+        Some(image::ImageFormat::Pnm) => "pnm".to_string(),
+        _ => {
+            return Err(PromptError::Operation(
+                "Unsupported image format.".to_string(),
+            ))
+        }
+    };
+
+    Ok(image_format)
 }
