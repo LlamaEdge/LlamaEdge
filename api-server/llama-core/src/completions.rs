773db295b5ed84225c2acd0cc5c628cdfa89b495
Xin Liu <sam@secondstate.io>
2024-03-14 02:48:17 +0900

    refactor!(core): support multi-models
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/llama-core/src/completions.rs
+++ api-server/llama-core/src/completions.rs
@@ -1,6 +1,6 @@
 use crate::{
     error::{BackendError, LlamaCoreError},
-    GRAPH, MAX_BUFFER_SIZE,
+    Graph, CHAT_GRAPHS, MAX_BUFFER_SIZE,
 };
 use endpoints::{
     common::{FinishReason, Usage},
@@ -14,7 +14,7 @@ pub async fn completions(request: &CompletionRequest) -> Result<CompletionObject
     // ! todo: a temp solution of computing the number of tokens in prompt
     let prompt_tokens = prompt.split_whitespace().count() as u64;
 
-    let buffer = infer(prompt.trim()).await?;
+    let buffer = infer(prompt.trim(), request.model.as_ref())?;
 
     // convert inference result to string
     let model_answer = String::from_utf8(buffer.clone()).map_err(|e| {
@@ -51,15 +51,63 @@ pub async fn completions(request: &CompletionRequest) -> Result<CompletionObject
     })
 }
 
-/// Runs inference on the model with the given name and returns the output.
-async fn infer(prompt: impl AsRef<str>) -> std::result::Result<Vec<u8>, LlamaCoreError> {
-    let graph = GRAPH.get().ok_or(LlamaCoreError::Operation(
-        "Fail to get the underlying value of `GRAPH`.".to_string(),
-    ))?;
-    let mut graph = graph.lock().map_err(|e| {
-        LlamaCoreError::Operation(format!("Fail to acquire the lock of `GRAPH`. {}", e))
-    })?;
+fn infer(
+    prompt: impl AsRef<str>,
+    model_name: Option<&String>,
+) -> std::result::Result<Vec<u8>, LlamaCoreError> {
+    match model_name {
+        Some(model_name) => {
+            let chat_graphs = CHAT_GRAPHS
+                .get()
+                .ok_or(LlamaCoreError::Operation(String::from(
+                    "Fail to get the underlying value of `CHAT_GRAPHS`.",
+                )))?;
+            let mut chat_graphs = chat_graphs.lock().map_err(|e| {
+                LlamaCoreError::Operation(format!(
+                    "Fail to acquire the lock of `CHAT_GRAPHS`. {}",
+                    e
+                ))
+            })?;
+            match chat_graphs.get_mut(model_name) {
+                Some(graph) => infer_by_graph(graph, prompt),
+                None => {
+                    return Err(LlamaCoreError::Operation(format!(
+                        "The model `{}` does not exist in the chat graphs.",
+                        &model_name
+                    )))
+                }
+            }
+        }
+        None => {
+            let chat_graphs = CHAT_GRAPHS
+                .get()
+                .ok_or(LlamaCoreError::Operation(String::from(
+                    "Fail to get the underlying value of `CHAT_GRAPHS`.",
+                )))?;
+            let mut chat_graphs = chat_graphs.lock().map_err(|e| {
+                LlamaCoreError::Operation(format!(
+                    "Fail to acquire the lock of `CHAT_GRAPHS`. {}",
+                    e
+                ))
+            })?;
 
+            match chat_graphs.iter_mut().next() {
+                Some((_, graph)) => infer_by_graph(graph, prompt),
+                None => {
+                    return Err(LlamaCoreError::Operation(String::from(
+                        "There is no model available in the chat graphs.",
+                    )))
+                }
+            }
+        }
+    }
+}
+
+/// Runs inference on the model with the given name and returns the output.
+fn infer_by_graph(
+    graph: &mut Graph,
+    prompt: impl AsRef<str>,
+) -> std::result::Result<Vec<u8>, LlamaCoreError> {
     // set input
     let tensor_data = prompt.as_ref().as_bytes().to_vec();
     graph
--- api-server/llama-core/src/models.rs
+++ api-server/llama-core/src/models.rs
@@ -1,21 +1,58 @@
-use crate::{error::LlamaCoreError, get_graph};
+use crate::{error::LlamaCoreError, CHAT_GRAPHS, EMBEDDING_GRAPHS};
 use endpoints::models::{ListModelsResponse, Model};
 
 /// Lists models available
 pub async fn models() -> Result<ListModelsResponse, LlamaCoreError> {
-    let graph = get_graph()?;
-    let created = graph.created.as_secs();
-    let id = graph.name.clone();
+    let mut models = vec![];
 
-    let model = Model {
-        id,
-        created,
-        object: String::from("model"),
-        owned_by: String::from("Not specified"),
-    };
+    {
+        let chat_graphs = CHAT_GRAPHS
+            .get()
+            .ok_or(LlamaCoreError::Operation(String::from(
+                "Fail to get the underlying value of `CHAT_GRAPHS`.",
+            )))?;
+
+        let chat_graphs = chat_graphs.lock().map_err(|e| {
+            LlamaCoreError::Operation(format!("Fail to acquire the lock of `CHAT_GRAPHS`. {}", e))
+        })?;
+
+        for (name, graph) in chat_graphs.iter() {
+            models.push(Model {
+                id: name.clone(),
+                created: graph.created.as_secs(),
+                object: String::from("model"),
+                owned_by: String::from("Not specified"),
+            });
+        }
+    }
+
+    {
+        let embedding_graphs =
+            EMBEDDING_GRAPHS
+                .get()
+                .ok_or(LlamaCoreError::Operation(String::from(
+                    "Fail to get the underlying value of `EMBEDDING_GRAPHS`.",
+                )))?;
+
+        let embedding_graphs = embedding_graphs.lock().map_err(|e| {
+            LlamaCoreError::Operation(format!(
+                "Fail to acquire the lock of `EMBEDDING_GRAPHS`. {}",
+                e
+            ))
+        })?;
+
+        for (name, graph) in embedding_graphs.iter() {
+            models.push(Model {
+                id: name.clone(),
+                created: graph.created.as_secs(),
+                object: String::from("model"),
+                owned_by: String::from("Not specified"),
+            });
+        }
+    }
 
     Ok(ListModelsResponse {
         object: String::from("list"),
-        data: vec![model],
+        data: models,
     })
 }
