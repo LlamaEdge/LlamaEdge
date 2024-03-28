74b7bd788ce70df3be94c165c53ee7183feb5d62
Xin Liu <sam@secondstate.io>
2024-03-28 13:59:01 +0900

    refactor(core): update `models` API
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/llama-core/src/models.rs
+++ api-server/llama-core/src/models.rs
@@ -27,27 +27,24 @@ pub async fn models() -> Result<ListModelsResponse, LlamaCoreError> {
     }
 
     {
-        let embedding_graphs =
-            EMBEDDING_GRAPHS
-                .get()
-                .ok_or(LlamaCoreError::Operation(String::from(
-                    "Fail to get the underlying value of `EMBEDDING_GRAPHS`.",
-                )))?;
+        if let Some(embedding_graphs) = EMBEDDING_GRAPHS.get() {
+            let embedding_graphs = embedding_graphs.lock().map_err(|e| {
+                LlamaCoreError::Operation(format!(
+                    "Fail to acquire the lock of `EMBEDDING_GRAPHS`. {}",
+                    e
+                ))
+            })?;
 
-        let embedding_graphs = embedding_graphs.lock().map_err(|e| {
-            LlamaCoreError::Operation(format!(
-                "Fail to acquire the lock of `EMBEDDING_GRAPHS`. {}",
-                e
-            ))
-        })?;
-
-        for (name, graph) in embedding_graphs.iter() {
-            models.push(Model {
-                id: name.clone(),
-                created: graph.created.as_secs(),
-                object: String::from("model"),
-                owned_by: String::from("Not specified"),
-            });
+            if !embedding_graphs.is_empty() {
+                for (name, graph) in embedding_graphs.iter() {
+                    models.push(Model {
+                        id: name.clone(),
+                        created: graph.created.as_secs(),
+                        object: String::from("model"),
+                        owned_by: String::from("Not specified"),
+                    });
+                }
+            }
         }
     }
 
