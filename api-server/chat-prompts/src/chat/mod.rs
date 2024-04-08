7a7922e0d9ac13390d535fc0080395be345db971
Xin Liu <sam@secondstate.io>
2024-04-08 16:58:46 +0800

    feat(prompts): implment `From<PromptTemplateType> for `ChatPrompt`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/chat-prompts/src/chat/mod.rs
+++ api-server/chat-prompts/src/chat/mod.rs
@@ -13,7 +13,7 @@ pub mod vicuna;
 pub mod wizard;
 pub mod zephyr;
 
-use crate::error::Result;
+use crate::{error::Result, PromptTemplateType};
 use baichuan::*;
 use belle::*;
 use chatml::*;
@@ -61,4 +61,48 @@ pub enum ChatPrompt {
     Phi2ChatPrompt,
     Phi2InstructPrompt,
     GemmaInstructPrompt,
+}
+impl From<PromptTemplateType> for ChatPrompt {
+    fn from(ty: PromptTemplateType) -> Self {
+        match ty {
+            PromptTemplateType::Llama2Chat => ChatPrompt::Llama2ChatPrompt(Llama2ChatPrompt),
+            PromptTemplateType::MistralInstruct => {
+                ChatPrompt::MistralInstructPrompt(MistralInstructPrompt)
+            }
+            PromptTemplateType::MistralLite => ChatPrompt::MistralLitePrompt(MistralLitePrompt),
+            PromptTemplateType::OpenChat => ChatPrompt::OpenChatPrompt(OpenChatPrompt),
+            PromptTemplateType::CodeLlama => {
+                ChatPrompt::CodeLlamaInstructPrompt(CodeLlamaInstructPrompt)
+            }
+            PromptTemplateType::CodeLlamaSuper => {
+                ChatPrompt::CodeLlamaSuperInstructPrompt(CodeLlamaSuperInstructPrompt)
+            }
+            PromptTemplateType::HumanAssistant => {
+                ChatPrompt::HumanAssistantChatPrompt(HumanAssistantChatPrompt)
+            }
+            PromptTemplateType::VicunaChat => ChatPrompt::VicunaChatPrompt(VicunaChatPrompt),
+            PromptTemplateType::Vicuna11Chat => ChatPrompt::Vicuna11ChatPrompt(Vicuna11ChatPrompt),
+            PromptTemplateType::VicunaLlava => ChatPrompt::VicunaLlavaPrompt(VicunaLlavaPrompt),
+            PromptTemplateType::ChatML => ChatPrompt::ChatMLPrompt(ChatMLPrompt),
+            PromptTemplateType::Baichuan2 => ChatPrompt::Baichuan2ChatPrompt(Baichuan2ChatPrompt),
+            PromptTemplateType::WizardCoder => ChatPrompt::WizardCoderPrompt(WizardCoderPrompt),
+            PromptTemplateType::Zephyr => ChatPrompt::ZephyrChatPrompt(ZephyrChatPrompt),
+            PromptTemplateType::StableLMZephyr => {
+                ChatPrompt::StableLMZephyrChatPrompt(StableLMZephyrChatPrompt)
+            }
+            PromptTemplateType::IntelNeural => ChatPrompt::NeuralChatPrompt(NeuralChatPrompt),
+            PromptTemplateType::DeepseekChat => ChatPrompt::DeepseekChatPrompt(DeepseekChatPrompt),
+            PromptTemplateType::DeepseekCoder => {
+                ChatPrompt::DeepseekCoderPrompt(DeepseekCoderPrompt)
+            }
+            PromptTemplateType::SolarInstruct => {
+                ChatPrompt::SolarInstructPrompt(SolarInstructPrompt)
+            }
+            PromptTemplateType::Phi2Chat => ChatPrompt::Phi2ChatPrompt(Phi2ChatPrompt),
+            PromptTemplateType::Phi2Instruct => ChatPrompt::Phi2InstructPrompt(Phi2InstructPrompt),
+            PromptTemplateType::GemmaInstruct => {
+                ChatPrompt::GemmaInstructPrompt(GemmaInstructPrompt)
+            }
+        }
+    }
 }
