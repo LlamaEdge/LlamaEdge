e617b4989ec477a539149227bdf18d8776500158
Xin Liu <sam@secondstate.io>
2024-05-23 15:26:27 +0800

    refactor!(llama-api-server): add subcommands in CLI
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/llama-api-server/src/main.rs
+++ api-server/llama-api-server/src/main.rs
@@ -4,7 +4,7 @@ mod utils;
 
 use anyhow::Result;
 use chat_prompts::PromptTemplateType;
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 use error::ServerError;
 use hyper::{
     header,
@@ -12,7 +12,7 @@ use hyper::{
     Body, Request, Response, Server, StatusCode,
 };
 use llama_core::MetadataBuilder;
-use std::{net::SocketAddr, path::PathBuf};
+use std::{net::SocketAddr, path::PathBuf, sync::Arc};
 use utils::log;
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
@@ -23,19 +23,39 @@ const DEFAULT_SOCKET_ADDRESS: &str = "0.0.0.0:8080";
 #[derive(Debug, Parser)]
 #[command(author, about, version, long_about=None)]
 struct Cli {
-    /// Model name
-    #[arg(short, long, default_value = "default")]
+    #[clap(subcommand)]
+    command: Commands,
+
+    /// Socket address of LlamaEdge API Server instance
+    #[arg(long, default_value = DEFAULT_SOCKET_ADDRESS)]
+    socket_addr: String,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Start server for chat completions
+    Chat(ChatArgs),
+    /// Start server for computing embeddings
+    Embedding(EmbeddingArgs),
+    /// Start server for both chat completions and computing embeddings
+    Max(FullArgs),
+}
+
+#[derive(Debug, Args)]
+struct ChatArgs {
+    /// Name of chat model
+    #[arg(long, required = true)]
     model_name: String,
-    /// Model alias
-    #[arg(short = 'a', long, default_value = "default")]
+    /// Alias for chat model
+    #[arg(long, default_value = "default")]
     model_alias: String,
-    /// Context size
+    /// Context size of chat model
     #[arg(short, long, default_value = "512")]
     ctx_size: u64,
-    /// Sets the prompt template.
+    /// Prompt template used for chat completions
     #[arg(short, long, value_parser = clap::value_parser!(PromptTemplateType), required = true)]
     prompt_template: PromptTemplateType,
-    /// Halt generation at PROMPT, return control.
+    /// Reverse prompt for stopping generation.
     #[arg(short, long)]
     reverse_prompt: Option<String>,
     /// Number of tokens to predict
@@ -74,9 +94,102 @@ struct Cli {
     /// Print all log information to stdout
     #[arg(long)]
     log_all: bool,
-    /// Socket address of LlamaEdge API Server instance
-    #[arg(long, default_value = DEFAULT_SOCKET_ADDRESS)]
-    socket_addr: String,
+    /// Root path for the Web UI files
+    #[arg(long, default_value = "chatbot-ui")]
+    web_ui: PathBuf,
+}
+
+#[derive(Debug, Args)]
+struct EmbeddingArgs {
+    /// Name of embedding model
+    #[arg(long, required = true)]
+    model_name: String,
+    /// Alias for embedding model
+    #[arg(long, default_value = "default")]
+    model_alias: String,
+    /// Context size of embedding model
+    #[arg(short, long, default_value = "512")]
+    ctx_size: u64,
+    /// Batch size for embedding model
+    #[arg(short, long, default_value = "512")]
+    batch_size: u64,
+    /// Print statistics to stdout
+    #[arg(long)]
+    log_stat: bool,
+    /// Print all log information to stdout
+    #[arg(long)]
+    log_all: bool,
+}
+
+#[derive(Debug, Args)]
+struct FullArgs {
+    // * Chat model arguments
+    /// Name of chat model
+    #[arg(long, required = true)]
+    model_name: String,
+    /// Alias for chat model
+    #[arg(long, default_value = "default")]
+    model_alias: String,
+    /// Context size of chat model
+    #[arg(long, default_value = "512")]
+    ctx_size: u64,
+    /// Prompt template used for chat completions
+    #[arg(long, value_parser = clap::value_parser!(PromptTemplateType), required = true)]
+    prompt_template: PromptTemplateType,
+    /// Reverse prompt for stopping generation.
+    #[arg(long)]
+    reverse_prompt: Option<String>,
+    /// Number of tokens to predict
+    #[arg(long, default_value = "1024")]
+    n_predict: u64,
+    /// Number of layers to run on the GPU
+    #[arg(long, default_value = "100")]
+    n_gpu_layers: u64,
+    /// Batch size for prompt processing
+    #[arg(long, default_value = "512")]
+    batch_size: u64,
+    /// Temperature for sampling
+    #[arg(long, default_value = "1.0")]
+    temp: f64,
+    /// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. 1.0 = disabled
+    #[arg(long, default_value = "1.0")]
+    top_p: f64,
+    /// Penalize repeat sequence of tokens
+    #[arg(long, default_value = "1.1")]
+    repeat_penalty: f64,
+    /// Repeat alpha presence penalty. 0.0 = disabled
+    #[arg(long, default_value = "0.0")]
+    presence_penalty: f64,
+    /// Repeat alpha frequency penalty. 0.0 = disabled
+    #[arg(long, default_value = "0.0")]
+    frequency_penalty: f64,
+    /// Path to the multimodal projector file
+    #[arg(long)]
+    llava_mmproj: Option<String>,
+
+    // * Embedding model arguments
+    /// Name of embedding model
+    #[arg(long, required = true)]
+    embedding_model_name: String,
+    /// Alias for embedding model
+    #[arg(long, default_value = "embedding")]
+    embedding_model_alias: String,
+    /// Context size of embedding model
+    #[arg(long, default_value = "512")]
+    embedding_ctx_size: u64,
+    /// Batch size for embedding model
+    #[arg(long, default_value = "512")]
+    embedding_batch_size: u64,
+
+    /// Print prompt strings to stdout
+    #[arg(long)]
+    log_prompts: bool,
+    /// Print statistics to stdout
+    #[arg(long)]
+    log_stat: bool,
+    /// Print all log information to stdout
+    #[arg(long)]
+    log_all: bool,
     /// Root path for the Web UI files
     #[arg(long, default_value = "chatbot-ui")]
     web_ui: PathBuf,
@@ -95,76 +208,249 @@ async fn main() -> Result<(), ServerError> {
     let cli = Cli::parse();
 
     // log the version of the server
-    log(format!(
-        "\n[INFO] LlamaEdge version: {}",
-        env!("CARGO_PKG_VERSION")
-    ));
+    let server_version = env!("CARGO_PKG_VERSION").to_string();
+    log(format!("[INFO] LlamaEdge-RAG version: {}", &server_version));
 
-    // log the cli options
-    log(format!("[INFO] Model name: {}", &cli.model_name));
-    log(format!("[INFO] Model alias: {}", &cli.model_alias));
-    log(format!("[INFO] Context size: {}", &cli.ctx_size));
-    log(format!("[INFO] Prompt template: {}", &cli.prompt_template));
-    if let Some(reverse_prompt) = &cli.reverse_prompt {
-        log(format!("[INFO] reverse prompt: {}", reverse_prompt));
-    }
-    log(format!(
-        "[INFO] Number of tokens to predict: {}",
-        &cli.n_predict
-    ));
-    log(format!(
-        "[INFO] Number of layers to run on the GPU: {}",
-        &cli.n_gpu_layers
-    ));
-    log(format!(
-        "[INFO] Batch size for prompt processing: {}",
-        &cli.batch_size
-    ));
-    log(format!("[INFO] Temperature for sampling: {}", &cli.temp));
-    log(format!(
-        "[INFO] Top-p sampling (1.0 = disabled): {}",
-        &cli.top_p
-    ));
-    log(format!(
-        "[INFO] Penalize repeat sequence of tokens: {}",
-        &cli.repeat_penalty
-    ));
-    log(format!(
-        "[INFO] Presence penalty (0.0 = disabled): {}",
-        &cli.presence_penalty
-    ));
-    log(format!(
-        "[INFO] Frequency penalty (0.0 = disabled): {}",
-        &cli.frequency_penalty
-    ));
-    if let Some(llava_mmproj) = &cli.llava_mmproj {
-        log(format!("[INFO] Multimodal projector: {}", llava_mmproj));
-    }
-    log(format!("[INFO] Enable prompt log: {}", &cli.log_prompts));
-    log(format!("[INFO] Enable plugin log: {}", &cli.log_stat));
-    log(format!("[INFO] Socket address: {}", &cli.socket_addr));
+    let web_ui = match cli.command {
+        Commands::Chat(chat_args) => {
+            // log
+            {
+                // log the cli options
+                log(format!("[INFO] Model name: {}", &chat_args.model_name));
+                log(format!("[INFO] Model alias: {}", &chat_args.model_alias));
+                log(format!("[INFO] Context size: {}", &chat_args.ctx_size));
+                log(format!("[INFO] Batch size: {}", &chat_args.batch_size));
+                log(format!(
+                    "[INFO] Prompt template: {}",
+                    &chat_args.prompt_template
+                ));
+                if let Some(reverse_prompt) = &chat_args.reverse_prompt {
+                    log(format!("[INFO] reverse prompt: {}", reverse_prompt));
+                }
+                log(format!(
+                    "[INFO] Number of tokens to predict: {}",
+                    &chat_args.n_predict
+                ));
+                log(format!(
+                    "[INFO] Number of layers to run on the GPU: {}",
+                    &chat_args.n_gpu_layers
+                ));
+                log(format!(
+                    "[INFO] Temperature for sampling: {}",
+                    &chat_args.temp
+                ));
+                log(format!(
+                    "[INFO] Top-p sampling (1.0 = disabled): {}",
+                    &chat_args.top_p
+                ));
+                log(format!(
+                    "[INFO] Penalize repeat sequence of tokens: {}",
+                    &chat_args.repeat_penalty
+                ));
+                log(format!(
+                    "[INFO] Presence penalty (0.0 = disabled): {}",
+                    &chat_args.presence_penalty
+                ));
+                log(format!(
+                    "[INFO] Frequency penalty (0.0 = disabled): {}",
+                    &chat_args.frequency_penalty
+                ));
+                if let Some(llava_mmproj) = &chat_args.llava_mmproj {
+                    log(format!("[INFO] Multimodal projector: {}", llava_mmproj));
+                }
+                log(format!(
+                    "[INFO] Enable prompt log: {}",
+                    &chat_args.log_prompts
+                ));
+                log(format!("[INFO] Enable plugin log: {}", &chat_args.log_stat));
+            }
 
-    // create a Metadata instance
-    let metadata = MetadataBuilder::new(cli.model_name, cli.model_alias, cli.prompt_template)
-        .with_ctx_size(cli.ctx_size)
-        .with_n_predict(cli.n_predict)
-        .with_n_gpu_layers(cli.n_gpu_layers)
-        .with_batch_size(cli.batch_size)
-        .with_temperature(cli.temp)
-        .with_top_p(cli.top_p)
-        .with_repeat_penalty(cli.repeat_penalty)
-        .with_presence_penalty(cli.presence_penalty)
-        .with_frequency_penalty(cli.frequency_penalty)
-        .with_reverse_prompt(cli.reverse_prompt)
-        .with_mmproj(cli.llava_mmproj.clone())
-        .enable_prompts_log(cli.log_prompts || cli.log_all)
-        .enable_plugin_log(cli.log_stat || cli.log_all)
-        .enable_debug_log(plugin_debug)
-        .build();
+            // create a Metadata instance
+            let metadata = MetadataBuilder::new(
+                chat_args.model_name,
+                chat_args.model_alias,
+                chat_args.prompt_template,
+            )
+            .with_ctx_size(chat_args.ctx_size)
+            .with_batch_size(chat_args.batch_size)
+            .with_n_predict(chat_args.n_predict)
+            .with_n_gpu_layers(chat_args.n_gpu_layers)
+            .with_temperature(chat_args.temp)
+            .with_top_p(chat_args.top_p)
+            .with_repeat_penalty(chat_args.repeat_penalty)
+            .with_presence_penalty(chat_args.presence_penalty)
+            .with_frequency_penalty(chat_args.frequency_penalty)
+            .with_reverse_prompt(chat_args.reverse_prompt)
+            .with_mmproj(chat_args.llava_mmproj.clone())
+            .enable_prompts_log(chat_args.log_prompts || chat_args.log_all)
+            .enable_plugin_log(chat_args.log_stat || chat_args.log_all || plugin_debug)
+            .enable_debug_log(plugin_debug)
+            .build();
 
-    // initialize the core context
-    llama_core::init_core_context(&[metadata])
-        .map_err(|e| ServerError::Operation(format!("{}", e)))?;
+            // initialize the core context
+            llama_core::init_core_context(Some(&[metadata]), None)
+                .map_err(|e| ServerError::Operation(format!("{}", e)))?;
+
+            chat_args.web_ui.to_string_lossy().to_string()
+        }
+        Commands::Embedding(embedding_args) => {
+            // log
+            {
+                // log the cli options
+                log(format!("[INFO] Model name: {}", &embedding_args.model_name));
+                log(format!(
+                    "[INFO] Model alias: {}",
+                    &embedding_args.model_alias
+                ));
+                log(format!("[INFO] Context size: {}", &embedding_args.ctx_size));
+                log(format!("[INFO] Batch size: {}", &embedding_args.batch_size));
+            }
+
+            // create metadata for embedding model
+            let metadata = MetadataBuilder::new(
+                embedding_args.model_name,
+                embedding_args.model_alias,
+                PromptTemplateType::Llama2Chat, // TODO: dummy value. Embedding model does not use prompt template
+            )
+            .with_ctx_size(embedding_args.ctx_size)
+            .with_batch_size(embedding_args.batch_size)
+            .enable_plugin_log(embedding_args.log_stat || embedding_args.log_all || plugin_debug)
+            .enable_debug_log(plugin_debug)
+            .build();
+
+            // initialize the core context
+            llama_core::init_core_context(None, Some(&[metadata]))
+                .map_err(|e| ServerError::Operation(format!("{}", e)))?;
+
+            String::new()
+        }
+        Commands::Max(full_args) => {
+            // log
+            {
+                // log the cli options
+                log(format!(
+                    "[INFO] Name of chat model: {}",
+                    &full_args.model_name
+                ));
+                log(format!(
+                    "[INFO] Name of embedding model: {}",
+                    &full_args.embedding_model_name
+                ));
+                log(format!(
+                    "[INFO] Alias for chat model: {}",
+                    &full_args.model_alias
+                ));
+                log(format!(
+                    "[INFO] Alias for embedding model: {}",
+                    &full_args.embedding_model_alias
+                ));
+                log(format!(
+                    "[INFO] Context size of chat model: {}",
+                    &full_args.ctx_size
+                ));
+                log(format!(
+                    "[INFO] Context size of embedding model: {}",
+                    &full_args.embedding_ctx_size
+                ));
+                log(format!(
+                    "[INFO] Batch size for chat model: {}",
+                    &full_args.batch_size
+                ));
+                log(format!(
+                    "[INFO] Batch size for embedding model: {}",
+                    &full_args.embedding_batch_size
+                ));
+                log(format!(
+                    "[INFO] Prompt template: {}",
+                    &full_args.prompt_template
+                ));
+                if let Some(reverse_prompt) = &full_args.reverse_prompt {
+                    log(format!("[INFO] reverse prompt: {}", reverse_prompt));
+                }
+                log(format!(
+                    "[INFO] Number of tokens to predict: {}",
+                    &full_args.n_predict
+                ));
+                log(format!(
+                    "[INFO] Number of layers to run on the GPU: {}",
+                    &full_args.n_gpu_layers
+                ));
+                log(format!(
+                    "[INFO] Temperature for sampling: {}",
+                    &full_args.temp
+                ));
+                log(format!(
+                    "[INFO] Top-p sampling (1.0 = disabled): {}",
+                    &full_args.top_p
+                ));
+                log(format!(
+                    "[INFO] Penalize repeat sequence of tokens: {}",
+                    &full_args.repeat_penalty
+                ));
+                log(format!(
+                    "[INFO] Presence penalty (0.0 = disabled): {}",
+                    &full_args.presence_penalty
+                ));
+                log(format!(
+                    "[INFO] Frequency penalty (0.0 = disabled): {}",
+                    &full_args.frequency_penalty
+                ));
+                if let Some(llava_mmproj) = &full_args.llava_mmproj {
+                    log(format!("[INFO] Multimodal projector: {}", llava_mmproj));
+                }
+                log(format!(
+                    "[INFO] Enable prompt log: {}",
+                    &full_args.log_prompts
+                ));
+                log(format!("[INFO] Enable plugin log: {}", &full_args.log_stat));
+            }
+
+            // create metadata for chat model
+            let metadata_chat = {
+                MetadataBuilder::new(
+                    full_args.model_name,
+                    full_args.model_alias,
+                    full_args.prompt_template,
+                )
+                .with_ctx_size(full_args.ctx_size)
+                .with_batch_size(full_args.batch_size)
+                .with_n_predict(full_args.n_predict)
+                .with_n_gpu_layers(full_args.n_gpu_layers)
+                .with_temperature(full_args.temp)
+                .with_top_p(full_args.top_p)
+                .with_repeat_penalty(full_args.repeat_penalty)
+                .with_presence_penalty(full_args.presence_penalty)
+                .with_frequency_penalty(full_args.frequency_penalty)
+                .with_reverse_prompt(full_args.reverse_prompt)
+                .with_mmproj(full_args.llava_mmproj.clone())
+                .enable_prompts_log(full_args.log_prompts || full_args.log_all)
+                .enable_plugin_log(full_args.log_stat || full_args.log_all || plugin_debug)
+                .enable_debug_log(plugin_debug)
+                .build()
+            };
+
+            // create metadata for embedding model
+            let metadata_embedding = {
+                MetadataBuilder::new(
+                    full_args.embedding_model_name,
+                    full_args.embedding_model_alias,
+                    PromptTemplateType::Llama2Chat, // TODO: dummy value. Embedding model does not use prompt template
+                )
+                .with_ctx_size(full_args.embedding_ctx_size)
+                .with_batch_size(full_args.embedding_batch_size)
+                .enable_plugin_log(full_args.log_stat || full_args.log_all || plugin_debug)
+                .enable_debug_log(plugin_debug)
+                .build()
+            };
+
+            // initialize the core context
+            llama_core::init_core_context(Some(&[metadata_chat]), Some(&[metadata_embedding]))
+                .map_err(|e| ServerError::Operation(format!("{}", e)))?;
+
+            full_args.web_ui.to_string_lossy().to_string()
+        }
+    };
+    let ref_web_ui = Arc::new(web_ui);
 
     // get the plugin version info
     let plugin_info =
@@ -176,9 +462,14 @@ async fn main() -> Result<(), ServerError> {
     ));
 
     let new_service = make_service_fn(move |_| {
-        let web_ui = cli.web_ui.to_string_lossy().to_string();
+        // let web_ui = cli.web_ui.to_string_lossy().to_string();
+        let web_ui = ref_web_ui.clone();
 
-        async move { Ok::<_, Error>(service_fn(move |req| handle_request(req, web_ui.clone()))) }
+        async move {
+            Ok::<_, Error>(service_fn(move |req| {
+                handle_request(req, (*web_ui).clone())
+            }))
+        }
     });
 
     // socket address
