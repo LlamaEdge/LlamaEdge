ec4dfdfdc2d73bce1fda1a7018fd38285425a517
Xin Liu <sam@secondstate.io>
2024-05-24 23:28:00 +0800

    chore(llama-api-server): remove unused `log` func
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/llama-api-server/src/utils.rs
+++ api-server/llama-api-server/src/utils.rs
@@ -1,9 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-pub(crate) fn log(msg: impl std::fmt::Display) {
-    println!("{}", msg);
-}
-
 pub(crate) fn gen_chat_id() -> String {
     format!("chatcmpl-{}", uuid::Uuid::new_v4())
 }
