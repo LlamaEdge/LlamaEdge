d38fa98c28450868ee03b2e0989fad402dec710e
Xin Liu <sam@secondstate.io>
2024-05-27 23:54:02 +0800

    chore(llama-api-server): update error funcs
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/llama-api-server/src/error.rs
+++ api-server/llama-api-server/src/error.rs
@@ -1,22 +1,10 @@
-use crate::utils::{LogLevel, NewLogRecord};
-use hyper::{body::HttpBody, Body, Response};
-use serde_json::json;
+use hyper::{Body, Response};
 use thiserror::Error;
 
 #[allow(dead_code)]
 pub(crate) fn not_implemented() -> Response<Body> {
     // log error
-    {
-        let record = NewLogRecord::new(
-            LogLevel::Error,
-            None,
-            json!({
-                "message": "501 Not Implemented",
-            }),
-        );
-        let message = serde_json::to_string(&record).unwrap();
-        error!("{}", &message);
-    }
+    error!(target: "response", "501 Not Implemented");
 
     Response::builder()
         .header("Access-Control-Allow-Origin", "*")
@@ -34,56 +22,15 @@ pub(crate) fn internal_server_error(msg: impl AsRef<str>) -> Response<Body> {
     };
 
     // log error
-    {
-        let record = NewLogRecord::new(
-            LogLevel::Error,
-            None,
-            json!({
-                "message": format!("{msg}", msg = err_msg),
-            }),
-        );
-        let message = serde_json::to_string(&record).unwrap();
-        error!("{}", &message);
-    }
+    error!(target: "response", "{}", &err_msg);
 
-    let response = Response::builder()
+    Response::builder()
         .header("Access-Control-Allow-Origin", "*")
         .header("Access-Control-Allow-Methods", "*")
         .header("Access-Control-Allow-Headers", "*")
         .status(hyper::StatusCode::INTERNAL_SERVER_ERROR)
         .body(Body::from(err_msg))
-        .unwrap();
-
-    // log
-    {
-        let status_code = response.status();
-        let response_version = format!("{:?}", response.version());
-        let response_body_size: u64 = response.body().size_hint().lower();
-        let response_status = status_code.as_u16();
-        let response_is_informational = status_code.is_informational();
-        let response_is_success = status_code.is_success();
-        let response_is_redirection = status_code.is_redirection();
-        let response_is_client_error = status_code.is_client_error();
-        let response_is_server_error = status_code.is_server_error();
-        let record = NewLogRecord::new(
-            LogLevel::Error,
-            None,
-            json!({
-                "version": response_version,
-                "body_size": response_body_size,
-                "status": response_status,
-                "is_informational": response_is_informational,
-                "is_success": response_is_success,
-                "is_redirection": response_is_redirection,
-                "is_client_error": response_is_client_error,
-                "is_server_error": response_is_server_error,
-            }),
-        );
-        let message = serde_json::to_string(&record).unwrap();
-        error!(target: "response", "{}", message);
-    }
-
-    response
+        .unwrap()
 }
 
 pub(crate) fn bad_request(msg: impl AsRef<str>) -> Response<Body> {
@@ -93,17 +40,7 @@ pub(crate) fn bad_request(msg: impl AsRef<str>) -> Response<Body> {
     };
 
     // log error
-    {
-        let record = NewLogRecord::new(
-            LogLevel::Error,
-            None,
-            json!({
-                "message": format!("{msg}", msg = err_msg),
-            }),
-        );
-        let message = serde_json::to_string(&record).unwrap();
-        error!("{}", &message);
-    }
+    error!(target: "response", "{}", &err_msg);
 
     Response::builder()
         .header("Access-Control-Allow-Origin", "*")
@@ -124,17 +61,7 @@ pub(crate) fn invalid_endpoint(msg: impl AsRef<str>) -> Response<Body> {
     };
 
     // log error
-    {
-        let record = NewLogRecord::new(
-            LogLevel::Error,
-            None,
-            json!({
-                "message": format!("{msg}", msg = err_msg),
-            }),
-        );
-        let message = serde_json::to_string(&record).unwrap();
-        error!("{}", &message);
-    }
+    error!(target: "response", "{}", &err_msg);
 
     Response::builder()
         .header("Access-Control-Allow-Origin", "*")
