44d0517c5790c7bb54361af2ec60e99b660721df
Michael Yuan <michael@secondstate.io>
2024-06-13 08:20:36 +0000

    A new docker approach to bundle model files with the runtime
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- docker/run.sh
+++ docker/run.sh
@@ -3,13 +3,17 @@
 source /root/.wasmedge/env
 
 chat_file_name=$1
-prompt_template=$2
-chat_ctx_size=$3
-embedding_file_name=$4
+embedding_file_name=$2
+prompt_template=$3
+chat_ctx_size=$4
 embedding_ctx_size=$5
 
-if [ -z "$embedding_file_name" ]; then
-    wasmedge --dir .:. --nn-preload default:GGML:AUTO:/models/$chat_file_name llama-api-server.wasm --prompt-template $prompt_template --ctx-size $chat_ctx_size --model-name $chat_file_name --socket-addr 0.0.0.0:8080
-else
-    wasmedge --dir .:. --nn-preload default:GGML:AUTO:/models/$chat_file_name --nn-preload embedding:GGML:AUTO:/models/$embedding_file_name llama-api-server.wasm --prompt-template $prompt_template,embedding --ctx-size $chat_ctx_size,$embedding_ctx_size --model-name $chat_file_name,$embedding_file_name --socket-addr 0.0.0.0:8080
+if [ -z "$chat_ctx_size" ]; then
+    chat_ctx_size=512
 fi
+
+if [ -z "$embedding_ctx_size" ]; then
+    embedding_ctx_size=256
+fi
+
+wasmedge --dir .:. --nn-preload default:GGML:AUTO:/models/$chat_file_name --nn-preload embedding:GGML:AUTO:/models/$embedding_file_name llama-api-server.wasm --prompt-template $prompt_template,embedding --ctx-size $chat_ctx_size,$embedding_ctx_size --model-name $chat_file_name,$embedding_file_name --socket-addr 0.0.0.0:8080
