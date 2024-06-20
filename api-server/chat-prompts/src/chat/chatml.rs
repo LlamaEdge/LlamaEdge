b0a5033606b7f6e15d52554860bc8de0cd7abb09
Xin Liu <sam@secondstate.io>
2024-06-20 23:49:52 +0800

    feat(prompts): support tool use in `ChatMLPrompt`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/chat-prompts/src/chat/chatml.rs
+++ api-server/chat-prompts/src/chat/chatml.rs
@@ -3,7 +3,7 @@ use crate::error::{PromptError, Result};
 use endpoints::chat::{
     ChatCompletionAssistantMessage, ChatCompletionRequestMessage, ChatCompletionSystemMessage,
     ChatCompletionToolMessage, ChatCompletionUserMessage, ChatCompletionUserMessageContent,
-    ContentPart,
+    ContentPart, Tool,
 };
 
 /// Generate prompts for the models using ChatML template.
@@ -19,6 +19,52 @@ impl ChatMLPrompt {
                 "<|im_start|>system\n{system_prompt}<|im_end|>",
                 system_prompt = content
             ),
+        }
+    }
+
+    fn create_system_prompt_tool(
+        &self,
+        message: &ChatCompletionSystemMessage,
+        tools: Option<&[Tool]>,
+    ) -> String {
+        let content = message.content();
+        match content.is_empty() {
+            true => match tools {
+                Some(tools) => {
+                    let available_tools = serde_json::to_string(tools).unwrap();
+                    let tools = format!("<tools> {} </tools>", available_tools);
+
+                    let begin = r#"<|im_start|>system\nYou are a function calling AI model. You are provided with function signatures within <tools></tools> XML tags. You may call one or more functions to assist with the user query. Don't make assumptions about what values to plug into functions. Here are the available tools:"#;
+
+                    let end = r#"Use the following pydantic model json schema for each tool call you will make: {"properties": {"arguments": {"title": "Arguments", "type": "object"}, "name": {"title": "Name", "type": "string"}}, "required": ["arguments", "name"], "title": "FunctionCall", "type": "object"} For each function call return a json object with function name and arguments within <tool_call></tool_call> XML tags as follows:\n<tool_call>\n{"arguments": <args-dict>, "name": <function-name>}\n</tool_call><|im_end|>"#;
+
+                    format!("{} {} {}", begin, tools, end)
+                }
+                None => {
+                    String::from("<|im_start|>system\nAnswer as concisely as possible.<|im_end|>")
+                }
+            },
+            false => match tools {
+                Some(tools) => {
+                    let available_tools = serde_json::to_string(tools).unwrap();
+                    let tools = format!("<tools> {} </tools>", available_tools);
+
+                    let begin = format!(
+                            "<|im_start|>system\n{system_prompt}\nYou are provided with function signatures within <tools></tools> XML tags. You may call one or more functions to assist with the user query. Don't make assumptions about what values to plug into functions. Here are the available tools:",
+                            system_prompt = content
+                        );
+
+                    let end = r#"Use the following pydantic model json schema for each tool call you will make: {"properties": {"arguments": {"title": "Arguments", "type": "object"}, "name": {"title": "Name", "type": "string"}}, "required": ["arguments", "name"], "title": "FunctionCall", "type": "object"} For each function call return a json object with function name and arguments within <tool_call></tool_call> XML tags as follows:\n<tool_call>\n{"arguments": <args-dict>, "name": <function-name>}\n</tool_call><|im_end|>"#;
+
+                    format!("{} {} {}", begin, tools, end)
+                }
+                None => {
+                    format!(
+                        "<|im_start|>system\n{system_prompt}<|im_end|>",
+                        system_prompt = content
+                    )
+                }
+            },
         }
     }
 
@@ -114,6 +160,60 @@ impl BuildChatPrompt for ChatMLPrompt {
         let system_prompt = match messages[0] {
             ChatCompletionRequestMessage::System(ref message) => self.create_system_prompt(message),
             _ => String::from("<|im_start|>system\nAnswer as concisely as possible.<|im_end|>"),
+        };
+
+        // append user/assistant messages
+        let mut prompt = String::new();
+        for message in messages {
+            match message {
+                ChatCompletionRequestMessage::User(message) => {
+                    prompt = self.append_user_message(&prompt, &system_prompt, message);
+                }
+                ChatCompletionRequestMessage::Assistant(message) => {
+                    prompt = self.append_assistant_message(&prompt, message)?;
+                }
+                ChatCompletionRequestMessage::Tool(message) => {
+                    prompt = self.append_tool_message(&prompt, message)?;
+                }
+                _ => continue,
+            }
+        }
+
+        prompt.push_str("\n<|im_start|>assistant");
+
+        Ok(prompt)
+    }
+
+    fn build_with_tools(
+        &self,
+        messages: &mut Vec<ChatCompletionRequestMessage>,
+        tools: Option<&[Tool]>,
+    ) -> Result<String> {
+        if messages.is_empty() {
+            return Err(crate::error::PromptError::NoMessages);
+        }
+
+        // system prompt
+        let system_prompt = match messages[0] {
+            ChatCompletionRequestMessage::System(ref message) => {
+                self.create_system_prompt_tool(message, tools)
+            }
+            // _ => String::from("<|im_start|>system\nAnswer as concisely as possible.<|im_end|>"),
+            _ => match tools {
+                Some(tools) => {
+                    let available_tools = serde_json::to_string(tools).unwrap();
+                    let tools = format!("<tools> {} </tools>", available_tools);
+
+                    let begin = r#"<|im_start|>system\nYou are a function calling AI model. You are provided with function signatures within <tools></tools> XML tags. You may call one or more functions to assist with the user query. Don't make assumptions about what values to plug into functions. Here are the available tools:"#;
+
+                    let end = r#"Use the following pydantic model json schema for each tool call you will make: {"properties": {"arguments": {"title": "Arguments", "type": "object"}, "name": {"title": "Name", "type": "string"}}, "required": ["arguments", "name"], "title": "FunctionCall", "type": "object"} For each function call return a json object with function name and arguments within <tool_call></tool_call> XML tags as follows:\n<tool_call>\n{"arguments": <args-dict>, "name": <function-name>}\n</tool_call><|im_end|>"#;
+
+                    format!("{} {} {}", begin, tools, end)
+                }
+                None => {
+                    String::from("<|im_start|>system\nAnswer as concisely as possible.<|im_end|>")
+                }
+            },
         };
 
         // append user/assistant messages
