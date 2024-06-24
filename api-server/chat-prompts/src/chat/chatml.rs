e90c757797b495256fd52871d51f4fd7ae2813f2
Xin Liu <sam@secondstate.io>
2024-06-24 21:37:44 +0800

    refactor(prompts): extract `ChatMLToolPrompt` from `ChatMLPrompt`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/chat-prompts/src/chat/chatml.rs
+++ api-server/chat-prompts/src/chat/chatml.rs
@@ -22,6 +22,139 @@ impl ChatMLPrompt {
         }
     }
 
+    /// Create a user prompt from a chat completion request message.
+    fn append_user_message(
+        &self,
+        chat_history: impl AsRef<str>,
+        system_prompt: impl AsRef<str>,
+        message: &ChatCompletionUserMessage,
+    ) -> String {
+        let content = match message.content() {
+            ChatCompletionUserMessageContent::Text(text) => text.to_string(),
+            ChatCompletionUserMessageContent::Parts(parts) => {
+                let mut content = String::new();
+                for part in parts {
+                    if let ContentPart::Text(text_content) = part {
+                        content.push_str(text_content.text());
+                        content.push('\n');
+                    }
+                }
+                content
+            }
+        };
+
+        match chat_history.as_ref().is_empty() {
+            true => match system_prompt.as_ref().is_empty() {
+                true => {
+                    format!(
+                        "<|im_start|>user\n{user_message}<|im_end|>",
+                        user_message = content.trim(),
+                    )
+                }
+                false => {
+                    format!(
+                        "{system_prompt}\n<|im_start|>user\n{user_message}<|im_end|>",
+                        system_prompt = system_prompt.as_ref().trim(),
+                        user_message = content.trim(),
+                    )
+                }
+            },
+            false => format!(
+                "{chat_history}\n<|im_start|>user\n{user_message}<|im_end|>",
+                chat_history = chat_history.as_ref().trim(),
+                user_message = content.trim(),
+            ),
+        }
+    }
+
+    /// create an assistant prompt from a chat completion request message.
+    fn append_assistant_message(
+        &self,
+        chat_history: impl AsRef<str>,
+        message: &ChatCompletionAssistantMessage,
+    ) -> Result<String> {
+        let content = match message.content() {
+            Some(content) => content.to_string(),
+            // Note that the content is optional if `tool_calls` is specified.
+            None => match message.tool_calls().is_some() {
+                true => String::new(),
+                false => return Err(PromptError::NoAssistantMessage),
+            },
+        };
+
+        Ok(format!(
+            "{chat_history}\n<|im_start|>assistant\n{assistant_message}<|im_end|>",
+            chat_history = chat_history.as_ref().trim(),
+            assistant_message = content.trim(),
+        ))
+    }
+
+    /// create a tool prompt from a chat completion request message.
+    fn append_tool_message(
+        &self,
+        chat_history: impl AsRef<str>,
+        message: &ChatCompletionToolMessage,
+    ) -> Result<String> {
+        let content = message.content();
+
+        Ok(format!(
+            "{chat_history}\n<|im_start|>tool\n{tool_message}<|im_end|>",
+            chat_history = chat_history.as_ref().trim(),
+            tool_message = content.trim(),
+        ))
+    }
+}
+impl BuildChatPrompt for ChatMLPrompt {
+    fn build(&self, messages: &mut Vec<ChatCompletionRequestMessage>) -> Result<String> {
+        if messages.is_empty() {
+            return Err(crate::error::PromptError::NoMessages);
+        }
+
+        // system prompt
+        let system_prompt = match messages[0] {
+            ChatCompletionRequestMessage::System(ref message) => self.create_system_prompt(message),
+            _ => String::from("<|im_start|>system\nAnswer as concisely as possible.<|im_end|>"),
+        };
+
+        // append user/assistant messages
+        let mut prompt = String::new();
+        for message in messages {
+            match message {
+                ChatCompletionRequestMessage::User(message) => {
+                    prompt = self.append_user_message(&prompt, &system_prompt, message);
+                }
+                ChatCompletionRequestMessage::Assistant(message) => {
+                    prompt = self.append_assistant_message(&prompt, message)?;
+                }
+                ChatCompletionRequestMessage::Tool(message) => {
+                    prompt = self.append_tool_message(&prompt, message)?;
+                }
+                _ => continue,
+            }
+        }
+
+        prompt.push_str("\n<|im_start|>assistant");
+
+        Ok(prompt)
+    }
+}
+
+/// Generate prompts for the models using ChatML template.
+#[derive(Debug, Default, Clone)]
+pub struct ChatMLToolPrompt;
+impl ChatMLToolPrompt {
+    /// Create a system prompt from a chat completion request message.
+    fn create_system_prompt(&self, message: &ChatCompletionSystemMessage) -> String {
+        let content = message.content();
+        match content.is_empty() {
+            true => String::from("<|im_start|>system\nAnswer as concisely as possible.<|im_end|>"),
+            false => format!(
+                "<|im_start|>system\n{system_prompt}<|im_end|>",
+                system_prompt = content
+            ),
+        }
+    }
+
     fn create_system_prompt_tool(
         &self,
         message: &ChatCompletionSystemMessage,
@@ -142,13 +275,13 @@ impl ChatMLPrompt {
         message: &ChatCompletionToolMessage,
     ) -> String {
         format!(
-            "{chat_history}\n<|im_start|>tool\n{tool_message}<|im_end|>",
+            "{chat_history}\n<|im_start|>tool\n<tool_response>\n{tool_message}\n</tool_response>\n<|im_end|>",
             chat_history = chat_history.as_ref().trim(),
             tool_message = message.content().trim(),
         )
     }
 }
-impl BuildChatPrompt for ChatMLPrompt {
+impl BuildChatPrompt for ChatMLToolPrompt {
     fn build(&self, messages: &mut Vec<ChatCompletionRequestMessage>) -> Result<String> {
         if messages.is_empty() {
             return Err(crate::error::PromptError::NoMessages);
@@ -196,7 +329,6 @@ impl BuildChatPrompt for ChatMLPrompt {
             ChatCompletionRequestMessage::System(ref message) => {
                 self.create_system_prompt_tool(message, tools)
             }
-            // _ => String::from("<|im_start|>system\nAnswer as concisely as possible.<|im_end|>"),
             _ => match tools {
                 Some(tools) => {
                     let available_tools = serde_json::to_string(tools).unwrap();
