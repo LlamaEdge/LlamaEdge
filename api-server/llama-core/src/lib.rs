60518154a5f5ffc606de42736dd74ad46f327bd5
Xin Liu <sam@secondstate.io>
2024-07-01 15:07:08 +0800

    chore(core): update rustdoc
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- api-server/llama-core/src/lib.rs
+++ api-server/llama-core/src/lib.rs
@@ -1,3 +1,5 @@
+//! Llama Core, abbreviated as `llama-core`, defines a set of APIs. Developers can utilize these APIs to build applications based on large models, such as chatbots, RAG, and more.
+
 #[cfg(feature = "logging")]
 #[macro_use]
 extern crate log;
