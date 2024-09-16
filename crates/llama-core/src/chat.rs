bff8847da7034059cc6f3e1e0fb7610ab820fa37
Xin Liu <sam@secondstate.io>
2024-09-16 16:02:25 +0900

    feat(core): extend `parse_tool_calls` function to support `nemotron-tool`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- crates/llama-core/src/chat.rs
+++ crates/llama-core/src/chat.rs
@@ -1198,6 +1198,9 @@ fn parse_tool_calls(
             }
         }
         PromptTemplateType::GroqLlama3Tool => {
+            #[cfg(feature = "logging")]
+            info!(target: "stdout", "raw input: {}", input);
+
             match regex::Regex::new(r"(?s)<tool_call>((.|\r|\n)*?)</tool_call>") {
                 Ok(re) => {
                     let mut values: Vec<serde_json::Value> = vec![];
@@ -1488,7 +1491,100 @@ fn parse_tool_calls(
             #[cfg(feature = "logging")]
             info!(target: "stdout", "raw input: {}", input);
 
-            unimplemented!("The tool use is not supported for the prompt template: NemotronTool.");
+            match regex::Regex::new(r"(?s)<toolcall>\s*(.*?)\s*</toolcall>") {
+                Ok(re) => {
+                    let mut values: Vec<serde_json::Value> = vec![];
+                    for cap in re.captures_iter(input) {
+                        #[cfg(feature = "logging")]
+                        info!(target: "stdout", "captured: {}", &cap[0]);
+
+                        #[cfg(feature = "logging")]
+                        info!(target: "stdout", "extracted: {}", &cap[1]);
+
+                        let matched = cap[1].trim();
+
+                        #[cfg(feature = "logging")]
+                        info!(target: "stdout", "captured: {}", matched);
+
+                        match serde_json::from_str::<serde_json::Value>(matched) {
+                            Ok(value) => values.push(value),
+                            Err(e) => {
+                                let err_msg = format!(
+                                    "Failed to deserialize generated tool calls. Reason: {}",
+                                    e
+                                );
+
+                                #[cfg(feature = "logging")]
+                                error!(target: "stdout", "{}", &err_msg);
+
+                                return Err(LlamaCoreError::Operation(err_msg));
+                            }
+                        }
+                    }
+
+                    let mut tool_calls: Vec<ToolCall> = vec![];
+                    for value in values.iter() {
+                        let name = match value.get("name") {
+                            Some(name) => name.to_string().replace("\"", ""),
+                            None => {
+                                let err_msg = format!(
+                                    "Failed to get the name of the function. Tool call: {:?}",
+                                    value
+                                );
+
+                                #[cfg(feature = "logging")]
+                                error!(target: "stdout", "{}", &err_msg);
+
+                                return Err(LlamaCoreError::Operation(err_msg));
+                            }
+                        };
+
+                        let arguments = match value.get("arguments") {
+                            Some(arguments) => arguments.to_string(),
+                            None => {
+                                let err_msg = format!(
+                                    "Failed to get the arguments of the function. Tool call: {:?}",
+                                    value
+                                );
+
+                                #[cfg(feature = "logging")]
+                                error!(target: "stdout", "{}", &err_msg);
+
+                                return Err(LlamaCoreError::Operation(err_msg));
+                            }
+                        };
+
+                        let function = Function { name, arguments };
+
+                        let tool_call = ToolCall {
+                            id: "call_abc123".to_string(),
+                            ty: "function".to_string(),
+                            function,
+                        };
+
+                        tool_calls.push(tool_call);
+                    }
+
+                    let parsed = ParseResult {
+                        raw: input.to_owned(),
+                        content: None,
+                        tool_calls,
+                    };
+
+                    #[cfg(feature = "logging")]
+                    info!(target: "stdout", "parsed result: {:?}", parsed);
+
+                    Ok(parsed)
+                }
+                Err(e) => {
+                    let err_msg = format!("Failed to create a regex pattern. Reason: {}", e);
+
+                    #[cfg(feature = "logging")]
+                    error!(target: "stdout", "{}", &err_msg);
+
+                    Err(LlamaCoreError::Operation(err_msg))
+                }
+            }
         }
         _ => Err(LlamaCoreError::Operation(format!(
             "The tool use is only supported for prompt templates: {}, {}, {}, {}, and {}.",
