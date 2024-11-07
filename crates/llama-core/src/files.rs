f11be3bad572eed2a564e2370d5f15ba0f323e68
Xin Liu <sam@secondstate.io>
2024-11-07 15:57:27 +0800

    feat(core): add `upload_file` API in `files` mod
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- crates/llama-core/src/files.rs
+++ crates/llama-core/src/files.rs
@@ -1,7 +1,165 @@
-use crate::error::LlamaCoreError;
+use crate::{error::LlamaCoreError, ARCHIVES_DIR};
 use endpoints::files::{DeleteFileStatus, FileObject, ListFilesResponse};
-use std::fs;
+use hyper::{body::to_bytes, Body, Method, Request};
+use multipart::server::{Multipart, ReadEntry, ReadEntryResult};
+use multipart_2021 as multipart;
+use std::{
+    fs::{self, File},
+    io::{Cursor, Read, Write},
+    path::Path,
+    time::SystemTime,
+};
 use walkdir::{DirEntry, WalkDir};
+
+/// Upload a file and return the file object.
+///
+/// # Arguments
+///
+/// * `req`: The incoming POST request.
+///
+/// # Returns
+///
+/// A `FileObject` instance.
+///
+/// # Usage
+///
+/// This API should be used in a API server, which is responsible for
+/// receiving the request and then forward the request to this API.
+pub async fn upload_file(req: Request<Body>) -> Result<FileObject, LlamaCoreError> {
+    if req.method() == Method::POST {
+        let boundary = "boundary=";
+
+        let boundary = req.headers().get("content-type").and_then(|ct| {
+            let ct = ct.to_str().ok()?;
+            let idx = ct.find(boundary)?;
+            Some(ct[idx + boundary.len()..].to_string())
+        });
+
+        let req_body = req.into_body();
+        let body_bytes = match to_bytes(req_body).await {
+            Ok(body_bytes) => body_bytes,
+            Err(e) => {
+                let err_msg = format!("Fail to read buffer from request body. {}", e);
+
+                // log
+                error!(target: "stdout", "{}", &err_msg);
+
+                return Err(LlamaCoreError::Operation(err_msg));
+            }
+        };
+
+        let cursor = Cursor::new(body_bytes.to_vec());
+
+        let mut multipart = Multipart::with_body(cursor, boundary.unwrap());
+
+        while let ReadEntryResult::Entry(mut field) = multipart.read_entry_mut() {
+            if &*field.headers.name == "file" {
+                let filename = match field.headers.filename {
+                    Some(filename) => filename,
+                    None => {
+                        let err_msg =
+                            "Failed to upload the target file. The filename is not provided.";
+
+                        // log
+                        error!(target: "stdout", "{}", &err_msg);
+
+                        return Err(LlamaCoreError::Operation(err_msg.to_string()));
+                    }
+                };
+
+                if !((filename).to_lowercase().ends_with(".txt")
+                    || (filename).to_lowercase().ends_with(".md"))
+                    || (filename).to_lowercase().ends_with(".png")
+                {
+                    let err_msg = format!(
+                        "Failed to upload the target file. Only files with 'txt' and 'md' extensions are supported. The file extension is {}.",
+                        &filename
+                    );
+
+                    // log
+                    error!(target: "stdout", "{}", &err_msg);
+
+                    return Err(LlamaCoreError::Operation(err_msg));
+                }
+
+                let mut buffer = Vec::new();
+                let size_in_bytes = match field.data.read_to_end(&mut buffer) {
+                    Ok(size_in_bytes) => size_in_bytes,
+                    Err(e) => {
+                        let err_msg = format!("Failed to read the target file. {}", e);
+
+                        // log
+                        error!(target: "stdout", "{}", &err_msg);
+
+                        return Err(LlamaCoreError::Operation(err_msg));
+                    }
+                };
+
+                // create a unique file id
+                let id = format!("file_{}", uuid::Uuid::new_v4());
+
+                // save the file
+                let path = Path::new(ARCHIVES_DIR);
+                if !path.exists() {
+                    fs::create_dir(path).unwrap();
+                }
+                let file_path = path.join(&id);
+                if !file_path.exists() {
+                    fs::create_dir(&file_path).unwrap();
+                }
+                let mut file = match File::create(file_path.join(&filename)) {
+                    Ok(file) => file,
+                    Err(e) => {
+                        let err_msg =
+                            format!("Failed to create archive document {}. {}", &filename, e);
+
+                        // log
+                        error!(target: "stdout", "{}", &err_msg);
+
+                        return Err(LlamaCoreError::Operation(err_msg));
+                    }
+                };
+                file.write_all(&buffer[..]).unwrap();
+
+                // log
+                info!(target: "stdout", "file_id: {}, file_name: {}", &id, &filename);
+
+                let created_at = match SystemTime::now().duration_since(std::time::UNIX_EPOCH) {
+                    Ok(n) => n.as_secs(),
+                    Err(_) => {
+                        let err_msg = "Failed to get the current time.";
+
+                        // log
+                        error!(target: "stdout", "{}", &err_msg);
+
+                        return Err(LlamaCoreError::Operation(err_msg.to_string()));
+                    }
+                };
+
+                // create a file object
+                return Ok(FileObject {
+                    id,
+                    bytes: size_in_bytes as u64,
+                    created_at,
+                    filename,
+                    object: "file".to_string(),
+                    purpose: "assistants".to_string(),
+                });
+            }
+        }
+
+        Err(LlamaCoreError::Operation(
+            "Failed to upload the target file. No file is provided.".to_string(),
+        ))
+    } else {
+        let err_msg = "Invalid HTTP Method.";
+
+        // log
+        error!(target: "stdout", "{}", &err_msg);
+
+        Err(LlamaCoreError::Operation(err_msg.to_string()))
+    }
+}
 
 /// Remove the target file by id.
 ///
@@ -13,7 +171,7 @@ use walkdir::{DirEntry, WalkDir};
 ///
 /// A `DeleteFileStatus` instance.
 pub fn remove_file(id: impl AsRef<str>) -> Result<DeleteFileStatus, LlamaCoreError> {
-    let root = format!("archives/{}", id.as_ref());
+    let root = format!("{}/{}", ARCHIVES_DIR, id.as_ref());
     let status = match fs::remove_dir_all(root) {
         Ok(_) => {
             #[cfg(feature = "logging")]
@@ -52,9 +210,11 @@ pub fn remove_file(id: impl AsRef<str>) -> Result<DeleteFileStatus, LlamaCoreErr
 ///
 /// A `ListFilesResponse` instance.
 pub fn list_files() -> Result<ListFilesResponse, LlamaCoreError> {
-    let root = "archives";
     let mut file_objects: Vec<FileObject> = Vec::new();
-    for entry in WalkDir::new(root).into_iter().filter_map(|e| e.ok()) {
+    for entry in WalkDir::new(ARCHIVES_DIR)
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
         if !is_hidden(&entry) && entry.path().is_file() {
             #[cfg(feature = "logging")]
             info!(target: "stdout", "archive file: {}", entry.path().display());
@@ -120,7 +280,7 @@ pub fn list_files() -> Result<ListFilesResponse, LlamaCoreError> {
 ///
 /// A `FileObject` instance.
 pub fn retrieve_file(id: impl AsRef<str>) -> Result<FileObject, LlamaCoreError> {
-    let root = format!("archives/{}", id.as_ref());
+    let root = format!("{}/{}", ARCHIVES_DIR, id.as_ref());
     for entry in WalkDir::new(root).into_iter().filter_map(|e| e.ok()) {
         if !is_hidden(&entry) && entry.path().is_file() {
             #[cfg(feature = "logging")]
