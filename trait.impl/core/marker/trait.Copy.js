b8eac3a6f1796f8ccc39493159b3685806a1686c
apepkuss <apepkuss@users.noreply.github.com>
2024-12-08 13:48:36 +0000

    deploy: 1d625203de9655330931f3a9be74422f17f4dd76

---
+++ trait.impl/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["llama_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"llama_core/enum.StableDiffusionTask.html\" title=\"enum llama_core::StableDiffusionTask\">StableDiffusionTask</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[302]}
