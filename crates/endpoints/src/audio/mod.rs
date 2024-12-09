571a597e30cd5ca69ba792c05d44bff73f398578
Xin Liu <sam@secondstate.io>
2024-12-09 13:56:47 +0800

    feat!(endpoints): add `whisper` feature
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- crates/endpoints/src/audio/mod.rs
+++ crates/endpoints/src/audio/mod.rs
@@ -1,5 +1,9 @@
 //! Define types for turning audio into text or text into audio.
 
 pub mod speech;
+#[cfg(feature = "whisper")]
+#[cfg_attr(docsrs, doc(cfg(feature = "whisper")))]
 pub mod transcription;
+#[cfg(feature = "whisper")]
+#[cfg_attr(docsrs, doc(cfg(feature = "whisper")))]
 pub mod translation;
