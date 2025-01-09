2300ea307c4171559e4c13d06613a0700e1c3502
apepkuss <apepkuss@users.noreply.github.com>
2025-01-09 08:29:38 +0000

    deploy: bc402d239d2016108972a3220273b4b0e9d49ada

---
+++ trait.impl/core/error/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["llama_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"llama_core/error/enum.BackendError.html\" title=\"enum llama_core::error::BackendError\">BackendError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"llama_core/error/enum.LlamaCoreError.html\" title=\"enum llama_core::error::LlamaCoreError\">LlamaCoreError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[580]}
