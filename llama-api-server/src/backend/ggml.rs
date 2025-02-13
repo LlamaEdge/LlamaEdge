7cf561fefcdbaf79054bf09d229bdbd20cace097
Xin Liu <sam@secondstate.io>
2025-02-13 18:10:51 +0800

    feat(llama-api-server): support `/v1/audio/speech` endpoint
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- llama-api-server/src/backend/ggml.rs
+++ llama-api-server/src/backend/ggml.rs
@@ -1,5 +1,6 @@
 use crate::{error, utils::gen_chat_id, SERVER_INFO};
 use endpoints::{
+    audio::speech::SpeechRequest,
     chat::ChatCompletionRequest,
     completions::CompletionRequest,
     embeddings::{ChunksRequest, ChunksResponse, EmbeddingRequest},
@@ -1208,6 +1209,96 @@ pub(crate) async fn server_info_handler() -> Response<Body> {
     };
 
     info!(target: "stdout", "Send the server info response.");
+
+    res
+}
+
+/// Create the audio speech.
+pub(crate) async fn audio_speech_handler(req: Request<Body>) -> Response<Body> {
+    // log
+    info!(target: "stdout", "Handling the coming audio speech request.");
+
+    if req.method().eq(&Method::OPTIONS) {
+        let result = Response::builder()
+            .header("Access-Control-Allow-Origin", "*")
+            .header("Access-Control-Allow-Methods", "*")
+            .header("Access-Control-Allow-Headers", "*")
+            .header("Content-Type", "application/json")
+            .body(Body::empty());
+
+        match result {
+            Ok(response) => return response,
+            Err(e) => {
+                let err_msg = e.to_string();
+
+                // log
+                error!(target: "stdout", "{}", &err_msg);
+
+                return error::internal_server_error(err_msg);
+            }
+        }
+    }
+
+    info!(target: "stdout", "Prepare the chat completion request.");
+
+    // parse request
+    let body_bytes = match to_bytes(req.into_body()).await {
+        Ok(body_bytes) => body_bytes,
+        Err(e) => {
+            let err_msg = format!("Fail to read buffer from request body. {}", e);
+
+            // log
+            error!(target: "stdout", "{}", &err_msg);
+
+            return error::internal_server_error(err_msg);
+        }
+    };
+    let speech_request: SpeechRequest = match serde_json::from_slice(&body_bytes) {
+        Ok(speech_request) => speech_request,
+        Err(e) => {
+            let err_msg = format!("Fail to deserialize speech request: {msg}", msg = e);
+
+            // log
+            error!(target: "stdout", "{}", &err_msg);
+
+            return error::bad_request(err_msg);
+        }
+    };
+
+    let audio_buffer = match llama_core::tts::create_speech(speech_request).await {
+        Ok(buffer) => buffer,
+        Err(e) => {
+            let err_msg = format!("Failed to transcribe the audio. {}", e);
+
+            // log
+            error!(target: "stdout", "{}", &err_msg);
+
+            return error::internal_server_error(err_msg);
+        }
+    };
+
+    // return response
+    let result = Response::builder()
+        .header("Access-Control-Allow-Origin", "*")
+        .header("Access-Control-Allow-Methods", "*")
+        .header("Access-Control-Allow-Headers", "*")
+        .header("Content-Type", "audio/wav")
+        .header("Content-Disposition", "attachment; filename=audio.wav")
+        .body(Body::from(audio_buffer));
+
+    let res = match result {
+        Ok(response) => response,
+        Err(e) => {
+            let err_msg = e.to_string();
+
+            // log
+            error!(target: "stdout", "{}", &err_msg);
+
+            error::internal_server_error(err_msg)
+        }
+    };
+
+    info!(target: "stdout", "Send the audio speech response");
 
     res
 }
