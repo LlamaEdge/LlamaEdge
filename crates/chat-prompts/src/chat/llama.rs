894511d95b399bb90769a95f81fbaa0ad5761703
Xin Liu <sam@secondstate.io>
2025-04-11 15:32:53 +0000

    feat(prompts): improve `Llama4ChatPrompt` to support tool call
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- crates/chat-prompts/src/chat/llama.rs
+++ crates/chat-prompts/src/chat/llama.rs
@@ -676,8 +676,8 @@ impl BuildChatPrompt for Llama3ToolPrompt {
 
 /// Generate prompts for the `Llama-4-chat` model.
 #[derive(Debug, Default, Clone)]
-pub struct Llama4ChatPrompt;
-impl Llama4ChatPrompt {
+pub struct Llama4ChatPromptOld;
+impl Llama4ChatPromptOld {
     /// Create a system prompt from a chat completion request message.
     fn create_system_prompt(&self, message: &ChatCompletionSystemMessage) -> String {
         let content = message.content();
@@ -726,12 +726,12 @@ impl Llama4ChatPrompt {
 
         match chat_history.as_ref().is_empty() {
             true => format!(
-                "{system_prompt}<|header_start|>user<|header_end|>\n\n{user_message}<|eot|>",
+                "{system_prompt}\n<|header_start|>user<|header_end|>\n\n{user_message}<|eot|>",
                 system_prompt = system_prompt.as_ref().trim(),
                 user_message = content.trim(),
             ),
             false => format!(
-                "{chat_history}<|header_start|>user<|header_end|>\n\n{user_message}<|eot|>",
+                "{chat_history}\n<|header_start|>user<|header_end|>\n\n{user_message}<|eot|>",
                 chat_history = chat_history.as_ref().trim(),
                 user_message = content.trim(),
             ),
@@ -745,16 +745,18 @@ impl Llama4ChatPrompt {
         message: &ChatCompletionAssistantMessage,
     ) -> Result<String> {
         let content = match message.content() {
-            Some(content) => content.to_string(),
+            Some(content) if !content.is_empty() => content.to_string(),
             // Note that the content is optional if `tool_calls` is specified.
-            None => match message.tool_calls().is_some() {
-                true => String::new(),
-                false => return Err(PromptError::NoAssistantMessage),
+            _ => match message.tool_calls() {
+                Some(tool_calls) if !tool_calls.is_empty() => {
+                    serde_json::to_string(&tool_calls[0].function).unwrap()
+                }
+                _ => return Err(PromptError::NoAssistantMessage),
             },
         };
 
         Ok(format!(
-            "{chat_history}<|header_start|>assistant<|header_end|>\n\n{assistant_message}<|eot|>",
+            "{chat_history}\n<|header_start|>assistant<|header_end|>\n\n{assistant_message}<|eot|>",
             chat_history = chat_history.as_ref().trim(),
             assistant_message = content.trim(),
         ))
@@ -767,13 +769,13 @@ impl Llama4ChatPrompt {
         message: &ChatCompletionToolMessage,
     ) -> String {
         format!(
-            "{chat_history}<|header_start|>ipython<|header_end|>\n\n{tool_result}<|eot|>",
+            "{chat_history}\n<|header_start|>ipython<|header_end|>\n\n{tool_result}<|eot|>",
             chat_history = chat_history.as_ref().trim(),
             tool_result = message.content().trim()
         )
     }
 }
-impl BuildChatPrompt for Llama4ChatPrompt {
+impl BuildChatPrompt for Llama4ChatPromptOld {
     fn build(&self, messages: &mut Vec<ChatCompletionRequestMessage>) -> Result<String> {
         if messages.is_empty() {
             return Err(crate::error::PromptError::NoMessages);
@@ -836,6 +838,237 @@ impl BuildChatPrompt for Llama4ChatPrompt {
                 ChatCompletionRequestMessage::User(message) => {
                     prompt = self.append_user_message(&prompt, &system_prompt, message);
                 }
+                ChatCompletionRequestMessage::Assistant(message) => {
+                    prompt = self.append_assistant_message(&prompt, message)?;
+                }
+                ChatCompletionRequestMessage::Tool(message) => {
+                    prompt = self.append_tool_message(&prompt, message);
+                }
+                _ => continue,
+            }
+        }
+
+        prompt.push_str("<|header_start|>assistant<|header_end|>");
+
+        Ok(prompt)
+    }
+}
+
+/// Generate prompts for the `Llama-4-chat` model.
+#[derive(Debug, Default, Clone)]
+pub struct Llama4ChatPrompt;
+impl Llama4ChatPrompt {
+    /// Create a system prompt from a chat completion request message.
+    fn create_system_prompt(&self, message: &ChatCompletionSystemMessage) -> String {
+        let content = message.content();
+        match content.is_empty() {
+            true => String::from("<|begin_of_text|><|header_start|>system<|header_end|>\n\nYou are a helpful, respectful and honest assistant. Always answer as short as possible, while being safe.<|eot|>"),
+            false =>format!(
+                "<|begin_of_text|><|header_start|>system<|header_end|>\n\n{system_prompt}<|eot|>", system_prompt=content
+            )
+        }
+    }
+
+    /// Create a user prompt from a chat completion request message.
+    fn append_user_message(
+        &self,
+        chat_history: impl AsRef<str>,
+        system_prompt: impl AsRef<str>,
+        message: &ChatCompletionUserMessage,
+    ) -> String {
+        let content = match message.content() {
+            ChatCompletionUserMessageContent::Text(text) => text.to_string(),
+            ChatCompletionUserMessageContent::Parts(parts) => {
+                let mut content = String::new();
+                for part in parts {
+                    if let ContentPart::Text(text_content) = part {
+                        content.push_str(text_content.text());
+                        content.push('\n');
+                    }
+                }
+                content
+            }
+        };
+
+        match chat_history.as_ref().is_empty() {
+            true => format!(
+                "{system_prompt}\n<|header_start|>user<|header_end|>\n\n{user_message}<|eot|>",
+                system_prompt = system_prompt.as_ref().trim(),
+                user_message = content.trim(),
+            ),
+            false => format!(
+                "{chat_history}\n<|header_start|>user<|header_end|>\n\n{user_message}<|eot|>",
+                chat_history = chat_history.as_ref().trim(),
+                user_message = content.trim(),
+            ),
+        }
+    }
+
+    /// Create a user prompt from a chat completion request message.
+    fn append_user_message_tools(
+        &self,
+        chat_history: impl AsRef<str>,
+        system_prompt: impl AsRef<str>,
+        message: &ChatCompletionUserMessage,
+        tools: &[Tool],
+        last: bool,
+    ) -> String {
+        let content = match message.content() {
+            ChatCompletionUserMessageContent::Text(text) => text.to_string(),
+            ChatCompletionUserMessageContent::Parts(parts) => {
+                let mut content = String::new();
+                for part in parts {
+                    if let ContentPart::Text(text_content) = part {
+                        content.push_str(text_content.text());
+                        content.push('\n');
+                    }
+                }
+                content
+            }
+        };
+
+        match chat_history.as_ref().is_empty() {
+            true => match last {
+                true => {
+                    format!(
+                        "{system_prompt}\n<|header_start|>user<|header_end|>\n\nGiven the following functions, please respond with a JSON for a function call with its proper arguments that best answers the given prompt.\n\nRespond in the format {{\"name\": function name, \"parameters\": dictionary of argument name and its value}}.\nDo not use variables.\n\n{available_tools}\n\nQuestion: {user_message}<|eot|>",
+                        system_prompt = system_prompt.as_ref().trim(),
+                        available_tools = serde_json::to_string_pretty(tools).unwrap(),
+                        user_message = content.trim(),
+                    )
+                }
+                false => format!(
+                    "{system_prompt}\n<|header_start|>user<|header_end|>\n\n{user_message}<|eot|>",
+                    system_prompt = system_prompt.as_ref().trim(),
+                    user_message = content.trim(),
+                ),
+            },
+            false => match last {
+                true => {
+                    format!(
+                        "{chat_history}\n<|header_start|>user<|header_end|>\n\nGiven the following functions, please respond with a JSON for a function call with its proper arguments that best answers the given prompt.\n\nRespond in the format {{\"name\": function name, \"parameters\": dictionary of argument name and its value}}.\nDo not use variables.\n\n{available_tools}\n\nQuestion: {user_message}<|eot|>",
+                        chat_history = chat_history.as_ref().trim(),
+                        available_tools = serde_json::to_string_pretty(tools).unwrap(),
+                        user_message = content.trim(),
+                    )
+                }
+                false => format!(
+                    "{chat_history}\n<|header_start|>user<|header_end|>\n\n{user_message}<|eot|>",
+                    chat_history = chat_history.as_ref().trim(),
+                    user_message = content.trim(),
+                ),
+            },
+        }
+    }
+
+    /// create an assistant prompt from a chat completion request message.
+    fn append_assistant_message(
+        &self,
+        chat_history: impl AsRef<str>,
+        message: &ChatCompletionAssistantMessage,
+    ) -> Result<String> {
+        let content = match message.content() {
+            Some(content) if !content.is_empty() => content.to_string(),
+            // Note that the content is optional if `tool_calls` is specified.
+            _ => match message.tool_calls() {
+                Some(tool_calls) if !tool_calls.is_empty() => {
+                    serde_json::to_string(&tool_calls[0].function).unwrap()
+                }
+                _ => return Err(PromptError::NoAssistantMessage),
+            },
+        };
+
+        Ok(format!(
+            "{chat_history}\n<|header_start|>assistant<|header_end|>\n\n{assistant_message}<|eot|>",
+            chat_history = chat_history.as_ref().trim(),
+            assistant_message = content.trim(),
+        ))
+    }
+
+    /// Create a tool prompt.
+    fn append_tool_message(
+        &self,
+        chat_history: impl AsRef<str>,
+        message: &ChatCompletionToolMessage,
+    ) -> String {
+        format!(
+            "{chat_history}\n<|header_start|>ipython<|header_end|>\n\n{tool_result}<|eot|>",
+            chat_history = chat_history.as_ref().trim(),
+            tool_result = message.content().trim()
+        )
+    }
+}
+impl BuildChatPrompt for Llama4ChatPrompt {
+    fn build(&self, messages: &mut Vec<ChatCompletionRequestMessage>) -> Result<String> {
+        if messages.is_empty() {
+            return Err(crate::error::PromptError::NoMessages);
+        }
+
+        // system prompt
+        let system_prompt = match messages[0] {
+            ChatCompletionRequestMessage::System(ref message) => {
+                self.create_system_prompt(message)
+            }
+            _ => String::from("<|begin_of_text|><|header_start|>system<|header_end|>\n\nYou are a helpful, respectful and honest assistant. Always answer as short as possible, while being safe.<|eot|>"),
+        };
+
+        // append user/assistant messages
+        let mut prompt = String::new();
+        for message in messages {
+            match message {
+                ChatCompletionRequestMessage::User(message) => {
+                    prompt = self.append_user_message(&prompt, &system_prompt, message);
+                }
+                ChatCompletionRequestMessage::Assistant(message) => {
+                    prompt = self.append_assistant_message(&prompt, message)?;
+                }
+                _ => continue,
+            }
+        }
+
+        prompt.push_str("<|header_start|>assistant<|header_end|>");
+
+        Ok(prompt)
+    }
+
+    fn build_with_tools(
+        &self,
+        messages: &mut Vec<ChatCompletionRequestMessage>,
+        tools: Option<&[endpoints::chat::Tool]>,
+    ) -> Result<String> {
+        if messages.is_empty() {
+            return Err(crate::error::PromptError::NoMessages);
+        }
+
+        // system prompt
+        let system_prompt = match messages[0] {
+            ChatCompletionRequestMessage::System(ref message) => {
+                self.create_system_prompt(message)
+            }
+            _ => String::from("<|begin_of_text|><|header_start|>system<|header_end|>\n\nYou are a helpful, respectful and honest assistant. Always answer as short as possible, while being safe.<|eot|>"),
+        };
+
+        let len = messages.len();
+
+        // append user/assistant/tool messages
+        let mut prompt = String::new();
+        for (idx, message) in messages.iter_mut().enumerate() {
+            match message {
+                ChatCompletionRequestMessage::User(message) => match tools {
+                    Some(available_tools) if !available_tools.is_empty() => {
+                        let last = idx == len - 1;
+                        prompt = self.append_user_message_tools(
+                            &prompt,
+                            &system_prompt,
+                            message,
+                            available_tools,
+                            last,
+                        );
+                    }
+                    _ => {
+                        prompt = self.append_user_message(&prompt, &system_prompt, message);
+                    }
+                },
                 ChatCompletionRequestMessage::Assistant(message) => {
                     prompt = self.append_assistant_message(&prompt, message)?;
                 }
