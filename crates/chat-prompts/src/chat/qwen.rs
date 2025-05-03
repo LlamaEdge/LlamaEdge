3736a08c4f8154d792c11ebf0f214f3aa3a683bf
Xin Liu <sam@secondstate.io>
2025-05-03 14:24:51 +0800

    Release 0.18.3 (#293)
    * chore: update `post_process` func
    
    Signed-off-by: Xin Liu <sam@secondstate.io>
    
    * feat(prompts): support tool calls in `Qwen3NoThinkPrompt`
    
    Signed-off-by: Xin Liu <sam@secondstate.io>
    
    * feat(core): support qwen3 tool calls
    
    Signed-off-by: Xin Liu <sam@secondstate.io>
    
    * version(prompts): bump to `0.28.1`
    
    Signed-off-by: Xin Liu <sam@secondstate.io>
    
    * version(core): bump to `0.32.3`
    
    Signed-off-by: Xin Liu <sam@secondstate.io>
    
    * version: bump to `0.18.3`
    
    Signed-off-by: Xin Liu <sam@secondstate.io>
    
    ---------
    
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- crates/chat-prompts/src/chat/qwen.rs
+++ crates/chat-prompts/src/chat/qwen.rs
@@ -5,7 +5,8 @@ use crate::{
 };
 use endpoints::chat::{
     ChatCompletionAssistantMessage, ChatCompletionRequestMessage, ChatCompletionSystemMessage,
-    ChatCompletionUserMessage, ChatCompletionUserMessageContent, ContentPart,
+    ChatCompletionToolMessage, ChatCompletionUserMessage, ChatCompletionUserMessageContent,
+    ContentPart, Tool,
 };
 
 /// Qwen2-vl Prompt Template
@@ -163,6 +164,118 @@ impl BuildChatPrompt for Qwen2vlPrompt {
 
 /// Generate prompts for the `Qwen3` models.
 #[derive(Debug, Default, Clone)]
+pub struct Qwen3NoThinkPromptOld;
+impl Qwen3NoThinkPromptOld {
+    /// Create a system prompt from a chat completion request message.
+    fn create_system_prompt(&self, message: &ChatCompletionSystemMessage) -> String {
+        let content = message.content();
+        match content.is_empty() {
+            true => String::from("<|im_start|>system\nYou are a helpful assistant. Answer questions as concisely as possible.<|im_end|>"),
+            false => format!("<|im_start|>system\n{content}<|im_end|>"),
+        }
+    }
+
+    /// Create a user prompt from a chat completion request message.
+    fn append_user_message(
+        &self,
+        chat_history: impl AsRef<str>,
+        system_prompt: impl AsRef<str>,
+        message: &ChatCompletionUserMessage,
+    ) -> String {
+        let content = match message.content() {
+            ChatCompletionUserMessageContent::Text(text) => text.to_string(),
+            ChatCompletionUserMessageContent::Parts(parts) => {
+                let mut content = String::new();
+                for part in parts {
+                    if let ContentPart::Text(text_content) = part {
+                        content.push_str(text_content.text());
+                        content.push('\n');
+                    }
+                }
+                content
+            }
+        };
+
+        match chat_history.as_ref().is_empty() {
+            true => match system_prompt.as_ref().is_empty() {
+                true => {
+                    format!(
+                        "<|im_start|>user\n{user_message}<|im_end|>",
+                        user_message = content.trim(),
+                    )
+                }
+                false => {
+                    format!(
+                        "{system_prompt}\n<|im_start|>user\n{user_message}<|im_end|>",
+                        system_prompt = system_prompt.as_ref().trim(),
+                        user_message = content.trim(),
+                    )
+                }
+            },
+            false => format!(
+                "{chat_history}\n<|im_start|>user\n{user_message}<|im_end|>",
+                chat_history = chat_history.as_ref().trim(),
+                user_message = content.trim(),
+            ),
+        }
+    }
+
+    /// create an assistant prompt from a chat completion request message.
+    fn append_assistant_message(
+        &self,
+        chat_history: impl AsRef<str>,
+        message: &ChatCompletionAssistantMessage,
+    ) -> Result<String> {
+        let content = match message.content() {
+            Some(content) => content.to_string(),
+            // Note that the content is optional if `tool_calls` is specified.
+            None => match message.tool_calls().is_some() {
+                true => String::new(),
+                false => return Err(PromptError::NoAssistantMessage),
+            },
+        };
+
+        Ok(format!(
+            "{chat_history}\n<|im_start|>assistant\n{assistant_message}<|im_end|>",
+            chat_history = chat_history.as_ref().trim(),
+            assistant_message = content.trim(),
+        ))
+    }
+}
+impl BuildChatPrompt for Qwen3NoThinkPromptOld {
+    fn build(&self, messages: &mut Vec<ChatCompletionRequestMessage>) -> Result<String> {
+        if messages.is_empty() {
+            return Err(crate::error::PromptError::NoMessages);
+        }
+
+        // system prompt
+        let system_prompt = match messages[0] {
+            ChatCompletionRequestMessage::System(ref message) => self.create_system_prompt(message),
+            _ => String::from("<|im_start|>system\nYou are a helpful assistant. Answer questions as concisely as possible.<|im_end|>"),
+        };
+
+        // append user/assistant messages
+        let mut prompt = String::new();
+        for message in messages {
+            match message {
+                ChatCompletionRequestMessage::User(message) => {
+                    prompt = self.append_user_message(&prompt, &system_prompt, message);
+                }
+                ChatCompletionRequestMessage::Assistant(message) => {
+                    prompt = self.append_assistant_message(&prompt, message)?;
+                }
+                _ => continue,
+            }
+        }
+
+        prompt.push_str("\n<|im_start|>assistant\n<think>/n/n</think>");
+
+        Ok(prompt)
+    }
+}
+
+/// Generate prompts for the `Qwen3` models in tool use scenario.
+#[derive(Debug, Default, Clone)]
 pub struct Qwen3NoThinkPrompt;
 impl Qwen3NoThinkPrompt {
     /// Create a system prompt from a chat completion request message.
@@ -240,6 +353,19 @@ impl Qwen3NoThinkPrompt {
             assistant_message = content.trim(),
         ))
     }
+
+    /// create a tool prompt from a chat completion request message.
+    fn append_tool_message(
+        &self,
+        chat_history: impl AsRef<str>,
+        message: &ChatCompletionToolMessage,
+    ) -> String {
+        format!(
+            "{chat_history}\n<|im_start|>user\n{tool_message}<|im_end|>",
+            chat_history = chat_history.as_ref().trim(),
+            tool_message = message.content().trim(),
+        )
+    }
 }
 impl BuildChatPrompt for Qwen3NoThinkPrompt {
     fn build(&self, messages: &mut Vec<ChatCompletionRequestMessage>) -> Result<String> {
@@ -262,6 +388,71 @@ impl BuildChatPrompt for Qwen3NoThinkPrompt {
                 }
                 ChatCompletionRequestMessage::Assistant(message) => {
                     prompt = self.append_assistant_message(&prompt, message)?;
+                }
+                _ => continue,
+            }
+        }
+
+        prompt.push_str("\n<|im_start|>assistant\n<think>/n/n</think>");
+
+        Ok(prompt)
+    }
+
+    fn build_with_tools(
+        &self,
+        messages: &mut Vec<ChatCompletionRequestMessage>,
+        tools: Option<&[Tool]>,
+    ) -> Result<String> {
+        if messages.is_empty() {
+            return Err(crate::error::PromptError::NoMessages);
+        }
+
+        // system prompt
+        let system_prompt = match messages[0] {
+            ChatCompletionRequestMessage::System(ref message) => match tools {
+                Some(tools) if !tools.is_empty() => {
+                    let available_tools = serde_json::to_string(tools).unwrap();
+                    let tools = format!("<tools>\n{available_tools}\n</tools>");
+
+                    let begin = r#"# Tools\n\nYou may call one or more functions to assist with the user query.\n\nYou are provided with function signatures within <tools></tools> XML tags:"#;
+
+                    let end = r#"For each function call, return a json object with function name and arguments within <tool_call></tool_call> XML tags:\n<tool_call>\n{"name": <function-name>, "arguments": <args-json-object>}\n</tool_call>"#;
+
+                    let content = message.content();
+                    match content.is_empty() {
+                        true => format!("<|im_start|>system\nYou are a helpful assistant. Answer questions as concisely as possible.\n\n{begin}\n{tools}\n\n{end}\n<|im_end|>"),
+                        false => format!("<|im_start|>system\n{content}\n\n{begin}\n{tools}\n\n{end}\n<|im_end|>"),
+                    }
+                }
+                _ => self.create_system_prompt(message),
+            },
+            _ => match tools {
+                Some(tools) if !tools.is_empty() => {
+                    let available_tools = serde_json::to_string(tools).unwrap();
+                    let tools = format!("<tools>\n{available_tools}\n</tools>");
+
+                    let begin = r#"# Tools\n\nYou may call one or more functions to assist with the user query.\n\nYou are provided with function signatures within <tools></tools> XML tags:"#;
+
+                    let end = r#"For each function call, return a json object with function name and arguments within <tool_call></tool_call> XML tags:\n<tool_call>\n{"name": <function-name>, "arguments": <args-json-object>}\n</tool_call>"#;
+
+                    format!("<|im_start|>system\nYou are a helpful assistant. Answer questions as concisely as possible.\n\n{begin}\n{tools}\n\n{end}\n<|im_end|>")
+                }
+                _ => String::from("<|im_start|>system\nYou are a helpful assistant. Answer questions as concisely as possible.<|im_end|>"),
+            },
+        };
+
+        // append user/assistant messages
+        let mut prompt = String::new();
+        for message in messages {
+            match message {
+                ChatCompletionRequestMessage::User(message) => {
+                    prompt = self.append_user_message(&prompt, &system_prompt, message);
+                }
+                ChatCompletionRequestMessage::Assistant(message) => {
+                    prompt = self.append_assistant_message(&prompt, message)?;
+                }
+                ChatCompletionRequestMessage::Tool(message) => {
+                    prompt = self.append_tool_message(&prompt, message);
                 }
                 _ => continue,
             }
