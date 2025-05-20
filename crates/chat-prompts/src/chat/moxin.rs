a9e083d8e9cf640139efaf27d5eb46f2246c709e
Xin Liu <sam@secondstate.io>
2025-05-20 10:21:17 +0800

    feat!(prompts): add `MoxinInstructPrompt`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- crates/chat-prompts/src/chat/moxin.rs
+++ crates/chat-prompts/src/chat/moxin.rs
@@ -116,3 +116,83 @@ impl BuildChatPrompt for MoxinChatPrompt {
         Ok(prompt)
     }
 }
+
+/// Generate prompts for the `moxin-chat-7b` model.
+#[derive(Debug, Default, Clone)]
+pub struct MoxinInstructPrompt;
+impl MoxinInstructPrompt {
+    /// Create a user prompt from a chat completion request message.
+    fn append_user_message(
+        &self,
+        chat_history: impl AsRef<str>,
+        message: &ChatCompletionUserMessage,
+    ) -> String {
+        let content = match message.content() {
+            ChatCompletionUserMessageContent::Text(text) => text.to_string(),
+            ChatCompletionUserMessageContent::Parts(parts) => {
+                let mut content = String::new();
+                for part in parts {
+                    if let ContentPart::Text(text_content) = part {
+                        content.push_str(text_content.text());
+                        content.push('\n');
+                    }
+                }
+                content
+            }
+        };
+
+        match chat_history.as_ref().is_empty() {
+            true => format!("<|user|>\n{user_message}", user_message = content.trim(),),
+            false => format!(
+                "{chat_history}\n<|user|>\n{user_message}",
+                chat_history = chat_history.as_ref().trim(),
+                user_message = content.trim(),
+            ),
+        }
+    }
+
+    /// create an assistant prompt from a chat completion request message.
+    fn append_assistant_message(
+        &self,
+        chat_history: impl AsRef<str>,
+        message: &ChatCompletionAssistantMessage,
+    ) -> Result<String> {
+        let content = match message.content() {
+            Some(content) => content.to_string(),
+            // Note that the content is optional if `tool_calls` is specified.
+            None => match message.tool_calls().is_some() {
+                true => String::new(),
+                false => return Err(PromptError::NoAssistantMessage),
+            },
+        };
+
+        Ok(format!(
+            "{chat_history}\n<|assistant|>\n{assistant_message}<|endoftext|>",
+            chat_history = chat_history.as_ref().trim(),
+            assistant_message = content.trim(),
+        ))
+    }
+}
+impl BuildChatPrompt for MoxinInstructPrompt {
+    fn build(&self, messages: &mut Vec<ChatCompletionRequestMessage>) -> Result<String> {
+        if messages.is_empty() {
+            return Err(crate::error::PromptError::NoMessages);
+        }
+
+        // append user/assistant messages
+        let mut prompt = String::new();
+        for message in messages {
+            match message {
+                ChatCompletionRequestMessage::User(message) => {
+                    prompt = self.append_user_message(&prompt, message);
+                }
+                ChatCompletionRequestMessage::Assistant(message) => {
+                    prompt = self.append_assistant_message(&prompt, message)?;
+                }
+                _ => continue,
+            }
+        }
+
+        Ok(prompt)
+    }
+}
