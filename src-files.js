f2b6fb86725cbb95933ae533b0d402a3b4fe6f3a
apepkuss <apepkuss@users.noreply.github.com>
2025-06-15 08:18:16 +0000

    deploy: b8a86c358e3332396b6a50f453906344b31f80b6

---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["llama_core",["",[["metadata",[],["ggml.rs","mod.rs","piper.rs","whisper.rs"]]],["audio.rs","chat.rs","completions.rs","embeddings.rs","error.rs","files.rs","graph.rs","images.rs","lib.rs","models.rs","rag.rs","search.rs","tts.rs","utils.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[245]}
