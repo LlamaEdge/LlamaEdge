8bc7eb14de7258c6fa256d0af2e691154015d4b1
Xin Liu <sam@secondstate.io>
2025-09-04 16:04:11 +0800

    refactor!(endpoints): remove `rag` and `index` features
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- crates/endpoints/src/rag/mod.rs
+++
@@ -1,7 +0,0 @@
-#[cfg(feature = "index")]
-#[cfg_attr(docsrs, doc(cfg(feature = "index")))]
-pub mod keyword_search;
-
-#[cfg(feature = "rag")]
-#[cfg_attr(docsrs, doc(cfg(feature = "rag")))]
-pub mod vector_search;
